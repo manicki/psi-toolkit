395834b0e63558683bcba6bbbe39db4787537cd0
Paweł Skórzewski <pawel.skorzewski@amu.edu.pl>
2011-07-12 09:46:32 +0200

    LayerTagManager and LayerTagCollection methods implemented.

--- common/layer_tag_collection.cpp
+++ common/layer_tag_collection.cpp
@@ -4,12 +4,36 @@ bool LayerTagCollection::isEmpty() {
     return false;
 }
 
+int LayerTagCollection::resize_(int newSize) {
+    int size = v_.size();
+    if (newSize > size) {
+        do {
+            size *= 2;
+        } while (newSize > size);
+        v_.resize(size);
+    }
+    return size;
+}
+
+int LayerTagCollection::resize_(LayerTagCollection other) {
+    if (v_.size() < other.v_.size()) {
+        return resize_(other.v_.size());
+    } else if (v_.size() > other.v_.size()) {
+        return other.resize_(v_.size());
+    }
+    return v_.size();
+}
+
 LayerTagCollection createUnion(LayerTagCollection tag_list_a, LayerTagCollection tag_list_b) {
-    tag_list_a.v_ |= tag_list_b.v_;
-    return tag_list_a;
+    LayerTagCollection result(tag_list_a.resize_(tag_list_b));
+    result.v_ |= tag_list_a.v_;
+    result.v_ |= tag_list_b.v_;
+    return result;
 }
 
 LayerTagCollection createIntersection(LayerTagCollection tag_list_a, LayerTagCollection tag_list_b) {
-    tag_list_a.v_ &= tag_list_b.v_;
-    return tag_list_a;
+    LayerTagCollection result(tag_list_a.resize_(tag_list_b));
+    result.v_ |= tag_list_a.v_;
+    result.v_ &= tag_list_b.v_;
+    return result;
 }
