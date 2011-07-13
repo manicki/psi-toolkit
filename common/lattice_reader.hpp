d9dd2975a7a08ac03affb9ba1e897e92c88e4084
Filip Graliński <filipg@amu.edu.pl>
2011-07-13 16:56:01 +0200

    LatticeReader abstract class added

---
+++ common/lattice_reader.hpp
@@ -0,0 +1,24 @@
+#ifndef LATTICE_READER_HDR
+#define LATTICE_READER_HDR
+
+#include <iostream>
+#include <string>
+
+
+class LatticeReader {
+
+public:
+    /**
+     * Reads from `inputStream` into `lattice`. If lattice is not empty,
+     * new edges will be appended at the end of lattice.
+     */
+    virtual void readIntoLattice(std::istream& inputStream, Lattice& lattice);
+
+    /**
+     * Gets format name (eg. "UTT", "BIN" etc.)
+     */
+    virtual std::string getFormatName();
+};
+
+
+#endif
