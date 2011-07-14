a52fd77ac87c76225a3288428e08343bf56093e9
Filip Gralinski <filipg@ceti.pl>
2011-07-14 23:27:48 +0200

    changed PSI format

---
+++ common/lattice_writer.hpp
@@ -0,0 +1,20 @@
+#ifndef LATTICE_WRITER_HDR
+#define LATTICE_WRITER_HDR
+
+#include <string>
+#include <ostream>
+
+#include "lattice.hpp"
+
+class LatticeWriter {
+
+public:
+    virtual void writeLattice(std::ostream& outputStream, Lattice& lattice) = 0;
+
+    /**
+     * Gets format name (eg. "UTT", "BIN" etc.)
+     */
+    virtual std::string getFormatName() = 0;
+}
+
+#endif
