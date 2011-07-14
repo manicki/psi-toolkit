d7823d1f26e5255e9ddac45be55eb4e5a5bb9b01
Paweł Skórzewski <pawel.skorzewski@amu.edu.pl>
2011-07-14 13:50:44 +0200

    UTT reader tests added

--- common/lattice_reader.hpp
+++ common/lattice_reader.hpp
@@ -1,7 +1,9 @@
 #ifndef LATTICE_READER_HDR
 #define LATTICE_READER_HDR
 
+#include <fstream>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 
 #include "lattice.hpp"
@@ -10,11 +12,26 @@
 class LatticeReader {
 
 public:
+
+    class CannotOpenFileException : public std::runtime_error {
+    public:
+        CannotOpenFileException() : std::runtime_error("cannot open a file") { }
+    };    
+
     /**
      * Reads from `inputStream` into `lattice`. If lattice is not empty,
      * new edges will be appended at the end of lattice.
      */
     virtual void readIntoLattice(std::istream& inputStream, Lattice& lattice) { }
+
+    /**
+     * Reads from file `fileName` into `lattice`. If lattice is not empty,
+     * new edges will be appended at the end of lattice.
+     */
+    void readIntoLattice(const std::string& fileName, Lattice& lattice) {
+        std::ifstream inputStream(fileName.c_str());
+        readIntoLattice(inputStream, lattice);
+    }
 
     /**
      * Gets format name (eg. "UTT", "BIN" etc.)
--- common/t/utt_reader_tests.hpp
+++ common/t/utt_reader_tests.hpp
@@ -6,7 +6,41 @@ class UTTReaderTests : public CxxTest::TestSuite {
 public:
 
     void test_simple() {
+        
         Lattice lattice("");
+        LatticeReader * reader = new UTTReader();
+        
+        reader->readIntoLattice("../../formats/utt/t/files/fr_simple_puddle_input.txt", lattice);
+        
+        LayerTagCollection tokenTag 
+            = lattice.getLayerTagManager().createSingletonTagCollection("token");
+        std::list<Lattice::EdgeDescriptor> edges = lattice.edgesSorted(tokenTag);
+        std::list<Lattice::EdgeDescriptor>::iterator ei = edges.begin();
+        
+        TS_ASSERT_EQUALS(lattice.getEdgeAnnotationItem(*ei).getCategory(), "A");
+        ++ei;
+        TS_ASSERT_EQUALS(lattice.getEdgeAnnotationItem(*ei).getCategory(), "l");
+        ++ei;
+        TS_ASSERT_EQUALS(lattice.getEdgeAnnotationItem(*ei).getCategory(), "a");
+        ++ei;
+        TS_ASSERT_EQUALS(lattice.getEdgeAnnotationItem(*ei).getCategory(), " ");
+        ++ei;
+        TS_ASSERT_EQUALS(lattice.getEdgeAnnotationItem(*ei).getCategory(), "m");
+        ++ei;
+        TS_ASSERT_EQUALS(lattice.getEdgeAnnotationItem(*ei).getCategory(), "a");
+        ++ei;
+        TS_ASSERT_EQUALS(lattice.getEdgeAnnotationItem(*ei).getCategory(), " ");
+        ++ei;
+        TS_ASSERT_EQUALS(lattice.getEdgeAnnotationItem(*ei).getCategory(), "k");
+        ++ei;
+        TS_ASSERT_EQUALS(lattice.getEdgeAnnotationItem(*ei).getCategory(), "o");
+        ++ei;
+        TS_ASSERT_EQUALS(lattice.getEdgeAnnotationItem(*ei).getCategory(), "t");
+        ++ei;
+        TS_ASSERT_EQUALS(lattice.getEdgeAnnotationItem(*ei).getCategory(), "a");
+        
+        delete reader;
+        
     }
 
 };
