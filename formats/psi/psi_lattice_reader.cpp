d13775086b978adb93965ec3af12451d382ab79f
Paweł Skórzewski <pawel.skorzewski@amu.edu.pl>
2011-07-19 16:00:51 +0200

    First sketch of Psi lattice reader

--- formats/psi/psi_lattice_reader.cpp
+++ formats/psi/psi_lattice_reader.cpp
@@ -2,7 +2,20 @@
 
 
 void PsiLatticeReader::readIntoLattice(std::istream& inputStream, Lattice& lattice) {
-    //TODO
+    PsiLRGrammar grammar;
+    std::string line;
+    while (std::getline(inputStream, line)) {
+        PsiLRItem item;
+        std::string::const_iterator begin = line.begin();
+        std::string::const_iterator end = line.end();
+        if (parse(begin, end, grammar, item)) {
+
+            item.unescape();
+
+std::cerr << item.ordinal << " " << item.text << std::endl;
+
+        }
+    }
 }
 
 std::string PsiLatticeReader::getFormatName() {
