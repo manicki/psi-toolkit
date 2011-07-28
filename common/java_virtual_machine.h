3424ef9a9050aadc03b612e55f8083deaeb66425
RomanG <snk987@gmail.com>
2011-07-28 11:14:09 +0200

    first version of JVM class

---
+++ common/java_virtual_machine.h
@@ -0,0 +1,30 @@
+#include <jni.h>
+#include <string>
+
+using std::string;
+
+class JavaVirtualMachine {
+
+	public:
+	static jint const JNI_VERSION;
+	static string javaClassPath;
+	static string javaLibraryPath;
+
+	JavaVM* getJVM();
+	JNIEnv* getENV();
+	static void setJavaPaths(string classPath, string libraryPath);
+
+	static JavaVirtualMachine* Instance();
+
+	private:
+	int inited;
+	JavaVM* javaVM;
+	JNIEnv* jniENV;
+	JNIEnv* create(JavaVM ** jvm);
+
+	static JavaVirtualMachine* jvmInstance;
+  JavaVirtualMachine();
+  JavaVirtualMachine(const JavaVirtualMachine&);
+  JavaVirtualMachine& operator= (const JavaVirtualMachine&);
+
+};
