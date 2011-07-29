acd1ae614f65ec7c58c568de88e4ef1219933fd3
Roman Grundkiewicz <snk987@gmail.com>
2011-07-29 15:09:50 +0200

    clean JVM class

--- common/java_virtual_machine.cpp
+++ common/java_virtual_machine.cpp
@@ -21,28 +21,28 @@ JavaVirtualMachine::JavaVirtualMachine() {	// constructor
 
 JNIEnv* JavaVirtualMachine::create(JavaVM ** jvm) {
 	JNIEnv *env;
-  JavaVMInitArgs vm_args;
-  JavaVMOption options[2];
+    JavaVMInitArgs vm_args;
+    JavaVMOption options[2];
   
 	string cp = "-Djava.class.path=" + string(javaClassPath);
 	options[0].optionString = (char *)cp.c_str();
 	string lp = "-Djava.library.path=" + string(javaLibraryPath);
 	options[1].optionString = (char *)lp.c_str();
 
-  vm_args.version = JNI_VERSION; 
-  vm_args.nOptions = 2;
-  vm_args.options = options;
-  vm_args.ignoreUnrecognized = 0;
+    vm_args.version = JNI_VERSION; 
+    vm_args.nOptions = 2;
+    vm_args.options = options;
+    vm_args.ignoreUnrecognized = 0;
       
-  int result = JNI_CreateJavaVM(jvm, (void**)&env, &vm_args);
-  if (result < 0) {
+    int result = JNI_CreateJavaVM(jvm, (void**)&env, &vm_args);
+    if (result < 0) {
 		// it should be an exception
-  	printf("Unable to Launch JVM\n");
+        printf("Unable to Launch JVM\n");
 	}
 	else {
 		inited = true;
 	}
-  return env;
+    return env;
 }
 
 JavaVM* JavaVirtualMachine::getJVM() {
@@ -52,11 +52,10 @@ JNIEnv* JavaVirtualMachine::getENV() {
 	return jniENV;
 }
 
-void JavaVirtualMachine::setJavaPaths(string classPath, string libraryPath) {
-	javaClassPath = classPath;
-	javaLibraryPath = libraryPath;
-}
 void JavaVirtualMachine::setJavaClassPath(string classPath) {
 	javaClassPath = classPath;
+}
+void JavaVirtualMachine::setJavaLibraryPath(string libraryPath) {
+	javaLibraryPath = libraryPath;
 }
 
--- common/java_virtual_machine.hpp
+++ common/java_virtual_machine.hpp
@@ -10,26 +10,26 @@ class JavaVirtualMachine {
 
 	public:
 	static jint const JNI_VERSION;
-	static string javaClassPath;
-	static string javaLibraryPath;
 
 	JavaVM* getJVM();
 	JNIEnv* getENV();
-	static void setJavaPaths(string classPath, string libraryPath);
 	static void setJavaClassPath(string);
+    static void setJavaLibraryPath(string);
 
 	static JavaVirtualMachine* Instance();
 
 	private:
+	static string javaClassPath;
+	static string javaLibraryPath;
 	bool inited;
 	JavaVM* javaVM;
 	JNIEnv* jniENV;
 	JNIEnv* create(JavaVM ** jvm);
 
 	static JavaVirtualMachine* jvmInstance;
-  JavaVirtualMachine();
-  JavaVirtualMachine(const JavaVirtualMachine&);
-  JavaVirtualMachine& operator= (const JavaVirtualMachine&);
+    JavaVirtualMachine();
+    JavaVirtualMachine(const JavaVirtualMachine&);
+    JavaVirtualMachine& operator= (const JavaVirtualMachine&);
 
 };
 #endif
