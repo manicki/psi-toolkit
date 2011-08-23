ffc837a51853f07d16f408423ac048e62a33f2ba
Leszek Manicki <leszekm@leszekm.(none)>
2011-08-23 12:46:43 +0200

    fixed wrong vertex indices in puddle group action

--- tools/parsers/puddle/group_action.cpp
+++ tools/parsers/puddle/group_action.cpp
@@ -111,9 +111,12 @@ bool GroupAction::apply(Lattice &lattice, int currentEntity,
 //    group->setLabel(this->group);
 
 //    TransitionInfo *edgeStart = util::getEdge(pg, currentEntity, realStart);
-    Lattice::VertexDescriptor startVertex = currentEntity + realStart;
-    Lattice::VertexDescriptor headVertex = currentEntity + realHead;
-    Lattice::VertexDescriptor endVertex = currentEntity + realEnd;
+    Lattice::VertexDescriptor startVertex = lattice::getVertex(
+            lattice, currentEntity + realStart);
+    Lattice::VertexDescriptor headVertex = lattice::getVertex(
+            lattice, currentEntity + realHead);
+    Lattice::VertexDescriptor endVertex = lattice::getVertex(
+            lattice, currentEntity + realEnd);
     //Lattice::EdgeDescriptor edgeStart2 = lattice.firstOutEdge(startVertex,
     //lattice.getLayerTagManager().getMask("lemma"));
     //std::cerr << lattice.getEdgeText(edgeStart2) << " " << lattice.getEdgeSource(edgeStart2) << " " << lattice.getEdgeTarget(edgeStart2) << " " << lattice.getEdgeBeginIndex(edgeStart2) << " " << lattice.getEdgeLength(edgeStart2) << std::endl;
