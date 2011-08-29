fa0be1a7ec0188d7db7fc09be9e44157b74178b9
Filip Graliński <filipg@amu.edu.pl>
2011-08-29 20:41:59 +0200

    fixes check.sh

--- check.sh
+++ check.sh
@@ -7,5 +7,5 @@ valgrind --xml=yes --xml-file=valgrind.xml ./runner
 xsltproc  ~/valgrind-reports-to-xunit/xslt/valgrind_transform.xsl valgrind.xml > valgrindTestResults.xml
 
 cd ..
-cppcheck -f --xml . --enable=all echo `find . -type d ! -path './.git*' ! -path './prj*' | perl -ne 'chomp; print "-I$_ "'`  2> cppcheck-result.xml
+cppcheck -D__cplusplus -f --xml . --enable=all echo `find . -type d ! -path './.git*' ! -path './prj*' | perl -ne 'chomp; print "-I$_ "'`  2> cppcheck-result.xml
 
