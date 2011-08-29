f8b8b124b72f01ff03ad1be0c382f0907fb2a935
Filip Graliński <filipg@amu.edu.pl>
2011-08-29 20:17:35 +0200

    checks in one script

---
+++ check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd prj
+./runner
+
+valgrind --xml=yes --xml-file=valgrind.xml ./runner
+xsltproc  ~/valgrind-reports-to-xunit/xslt/valgrind_transform.xsl valgrind.xml > valgrindTestResults.xml
+
+cd ..
+cppcheck -f --xml . --enable=all echo `find . -type d ! -path './.git*' ! -path './prj*' | perl -ne 'chomp; print "-I$_ "'`  2> cppcheck-result.xml
+
