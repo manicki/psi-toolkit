34560c818ece5e2409fa444a0a60dd9b020f1c9b
Paweł Skórzewski <pawel.skorzewski@amu.edu.pl>
2011-08-30 12:45:20 +0200

    getVertexForRawCharIndex range control

--- common/lattice.cpp
+++ common/lattice.cpp
@@ -61,6 +61,12 @@ Lattice::VertexDescriptor Lattice::addLooseVertex() {
 }
 
 Lattice::VertexDescriptor Lattice::getVertexForRawCharIndex(int ix) {
+    if (ix < 0) {
+        throw NoVertexException("Raw char index below zero is incorrect.");
+    }
+    if (ix > allText_.length()) {
+        throw NoVertexException("Raw char index above text length is incorrect.");
+    }
     return ix;
 }
 
