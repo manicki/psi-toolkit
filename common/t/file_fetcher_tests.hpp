1af450a3de0425b0ae75796a09052180777afcab
Filip Graliński <filipg@amu.edu.pl>
2011-09-06 10:31:28 +0200

    added tests

--- common/t/file_fetcher_tests.hpp
+++ common/t/file_fetcher_tests.hpp
@@ -1,4 +1,5 @@
 #include "file_fetcher.hpp"
+#include "lang_specific_processor_file_fetcher.hpp"
 
 class FileFetcherTests : public CxxTest::TestSuite
 {
@@ -28,6 +29,68 @@ public:
                          "../common/t/ant_like_path_glob_tests_dir/foo/bar/baz/testB.txt");
 
         TS_ASSERT_THROWS(fileFetcher.getOneFile(fileSpec), FileFetcher::Exception);
+    }
+
+    void testSingleFile() {
+        FileFetcher fileFetcher;
+
+        fileFetcher.addParam(
+            "MAIN",
+            ROOT_DIR "common/t/ant_like_path_glob_tests_dir");
+        fileFetcher.addParam("NAME", "bla");
+
+        std::string fileSpec = "%MAIN%/*/%NAME%.txt";
+
+        TS_ASSERT_EQUALS(
+            fileFetcher.getOneFile(fileSpec).string(),
+            "../common/t/ant_like_path_glob_tests_dir/foo/bla.txt");
+    }
+
+    void testProcessorFetcher() {
+        // real file must be used
+        LangSpecificProcessorFileFetcher fileFetcher(
+            ROOT_DIR "tools/tokenizers/tp/tp_basic_tokenizer_rule_set.cpp",
+            "pl");
+
+        fileFetcher.addParam(
+            "MAIN",
+            ROOT_DIR "common/t/ant_like_path_glob_tests_dir");
+        fileFetcher.addParam("NAME", "bla");
+
+        TS_ASSERT_EQUALS(
+            fileFetcher.getOneFile("%ITSDATA%/%LANG%/%LANG%.rgx"),
+            "../tools/tokenizers/tp/data/pl/pl.rgx");
+
+        std::vector<boost::filesystem::path> filesFound;
+
+        fileFetcher.getFiles(
+            "%ITSDATA%/**/%LANG%.rgx,%ITSDATA%/xx/xx.rgx,%MAIN%/*/%NAME%.txt",
+            std::back_inserter(filesFound));
+
+        TS_ASSERT_EQUALS(filesFound.size(), 3U);
+
+        TS_ASSERT_EQUALS(
+            filesFound[0].string(),
+            "../common/t/ant_like_path_glob_tests_dir/foo/bla.txt");
+
+        TS_ASSERT_EQUALS(
+            filesFound[1].string(),
+            "../tools/tokenizers/tp/data/pl/pl.rgx");
+
+        TS_ASSERT_EQUALS(
+            filesFound[2].string(),
+            "../tools/tokenizers/tp/data/xx/xx.rgx");
+        
+    }
+
+    void testExceptions() {
+        FileFetcher fileFetcher;
+
+        std::vector<boost::filesystem::path> filesFound;
+        
+        TS_ASSERT_THROWS( 
+            fileFetcher.getFiles("%FOO%", std::back_inserter(filesFound)),
+            FileFetcher::Exception);
     }
 };
 
