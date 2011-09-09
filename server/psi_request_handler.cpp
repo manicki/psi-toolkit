5a2453cf5b803e8658db5e446c2c40647a2abcf3
Roman Grundkiewicz <romang@amu.edu.pl>
2011-09-09 12:57:59 +0200

    Added proper setting MIME types in PsiServer

--- server/psi_request_handler.cpp
+++ server/psi_request_handler.cpp
@@ -2,6 +2,8 @@
 
 #include <boost/lexical_cast.hpp>
 
+#include <iostream>
+
 void PsiRequestHandler::handle_request(
 	const http::server3::request& req, http::server3::reply& rep
 ) {
@@ -14,6 +16,25 @@ void PsiRequestHandler::handle_request(
 	psi_server_->include(rep.content);
 		
 	rep.headers[0].value = boost::lexical_cast<std::string>(rep.content.size());
-	rep.headers[1].value = "text/html;charset=UTF-8"; 
+	rep.headers[1].value = getContentType(req.headers);
+
+
 }
 
+std::string PsiRequestHandler::getContentType(
+	const std::vector<http::server3::header>& headers
+) {
+	
+	std::string str = headers[2].value;
+	size_t found = str.find(',');
+
+  	if (found != std::string::npos) {
+		str = str.substr(0, found);
+	} else {
+		str = "text/html";
+	}
+
+	str += ";charset=UTF-8";
+	
+	return str;
+}
