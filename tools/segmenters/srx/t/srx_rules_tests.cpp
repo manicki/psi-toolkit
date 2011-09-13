095cf880dc83ecf276091fadfbdb226f6aeb4b35
Filip Graliński <filipg@amu.edu.pl>
2011-09-13 20:11:08 +0200

    Boost tests for srx-segmenter

---
+++ tools/segmenters/srx/t/srx_rules_tests.cpp
@@ -0,0 +1,52 @@
+#define BOOST_TEST_NO_MAIN
+#include <boost/test/unit_test.hpp>
+
+#include "../srx_rules.hpp"
+
+#include "config.h"
+
+#include <vector>
+
+BOOST_AUTO_TEST_CASE( srx_simple ) {
+
+    std::vector<SrxRule> srxRules;
+
+    SrxRulesReader rulesReader(
+        ROOT_DIR "tools/segmenters/srx/t/simple.srx",
+        "pl");
+
+    rulesReader.getRules(std::back_inserter(srxRules));
+
+    BOOST_CHECK_EQUAL(srxRules.size(), 4U);
+
+    BOOST_CHECK_EQUAL(srxRules[0].isBreakable(), false);
+    BOOST_CHECK_EQUAL(srxRules[0].getBeforeBreak(), "^\\s*[0-9]+\\.");
+    BOOST_CHECK_EQUAL(srxRules[0].getAfterBreak(), "\\s");
+
+    BOOST_CHECK_EQUAL(srxRules[1].isBreakable(), false);
+    BOOST_CHECK_EQUAL(srxRules[1].getBeforeBreak(), "[Nn][Pp]\\.");
+    BOOST_CHECK_EQUAL(srxRules[1].getAfterBreak(), "");
+
+    BOOST_CHECK_EQUAL(srxRules[2].isBreakable(), true);
+    BOOST_CHECK_EQUAL(srxRules[2].getBeforeBreak(), "[\\.\\?!]+");
+    BOOST_CHECK_EQUAL(srxRules[2].getAfterBreak(), "\\s");
+
+    BOOST_CHECK_EQUAL(srxRules[3].isBreakable(), true);
+    BOOST_CHECK_EQUAL(srxRules[3].getBeforeBreak(), "");
+    BOOST_CHECK_EQUAL(srxRules[3].getAfterBreak(), "\\n");
+}
+
+BOOST_AUTO_TEST_CASE( real_one ) {
+
+    std::vector<SrxRule> srxRules;
+
+    SrxRulesReader rulesReader(
+        ROOT_DIR "tools/segmenters/srx/data/pl/segmentation.srx",
+        "pl");
+
+    rulesReader.getRules(std::back_inserter(srxRules));
+
+    BOOST_CHECK(1U < srxRules.size());
+}
+
+
