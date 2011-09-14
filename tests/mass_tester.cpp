bd0e6d1ed0378db8dfe28c651a2294fc7c9b60e4
Filip Gralinski <filipg@ceti.pl>
2011-09-14 10:38:43 +0200

    mass-tests cntd.

--- tests/mass_tester.cpp
+++ tests/mass_tester.cpp
@@ -2,7 +2,9 @@
 
 #include <boost/program_options/parsers.hpp>
 #include <boost/program_options/variables_map.hpp>
+#include <boost/filesystem/fstream.hpp>
 #include <boost/foreach.hpp>
+
 
 #include "logging.hpp"
 
@@ -30,8 +32,10 @@ MassTester::MassTester(int argc, char* argv[]) {
 
 int MassTester::run() {
     BOOST_FOREACH(boost::filesystem::path path, directories_) {
-        INFO("will look in " << path);
+        lookForTestBatches_(path);
     }
+
+    runAllBatches_();
 
     return 0;
 }
@@ -48,4 +52,78 @@ boost::program_options::options_description MassTester::optionsHandled() {
     return optionsDescription;
 }
 
+void MassTester::lookForTestBatches_(const boost::filesystem::path& directory) {
+    if (!boost::filesystem::is_directory(directory)) {
+        WARN(directory << "is not a directory");
+        return;
+    }
+
+    INFO("looking in " << directory);
+
+    boost::filesystem::recursive_directory_iterator end_itr;
+    for (boost::filesystem::recursive_directory_iterator fiter(directory);
+         fiter != end_itr;
+         ++fiter) {
+        boost::filesystem::path filename(fiter->path().filename());
+        if (boost::filesystem::is_directory(fiter->path())
+            && filename == TEST_BATCH_DIRECTORY_NAME)
+            lookInDirectory_(fiter->path());
+    }
+}
+
+void MassTester::lookInDirectory_(const boost::filesystem::path& directory) {
+    boost::filesystem::directory_iterator end_iter;
+    for (boost::filesystem::directory_iterator fiter(directory);
+             fiter != end_iter;
+             ++fiter)
+        addTestBatch_(fiter->path());
+}
+
+void MassTester::addTestBatch_(const boost::filesystem::path& directory) {
+    INFO("registering test batch " << directory);
+
+    boost::filesystem::path commandFileName = directory / TEST_COMMAND_FILE_NAME;
+
+    if (!boost::filesystem::is_regular_file(commandFileName)) {
+        WARN("no " << commandFileName << " found");
+    }
+
+    TestBatch testBatch(directory, readCommand_(commandFileName));
+    testBatches_.push_back(testBatch);
+}
+
+void MassTester::runAllBatches_() {
+    BOOST_FOREACH(TestBatch batch, testBatches_) {
+        runBatch_(batch);
+    }
+}
+
+void MassTester::runBatch_(const TestBatch& batch) {
+    INFO("running " << batch.getDirectory() << " [" << batch.getPipeline() << "]");
+}
+
+std::string MassTester::readCommand_(boost::filesystem::path commandFileName) {
+    boost::filesystem::ifstream commandStream(commandFileName);
+
+    std::string command;
+
+    while (commandStream) {
+        std::string line;
+        std::getline(commandStream, line);
+        line = stripComments_(line);
+        command += line;
+    }
+
+    return command;
+}
+
+std::string MassTester::stripComments_(const std::string& line) {
+    size_t hashPosition = line.find('#');
+    if (hashPosition != std::string::npos)
+        return line.substr(hashPosition);
+    return line;
+}
+
+const boost::filesystem::path MassTester::TEST_BATCH_DIRECTORY_NAME = "m";
+const boost::filesystem::path MassTester::TEST_COMMAND_FILE_NAME = "WHAT";
 
--- tests/mass_tester.hpp
+++ tests/mass_tester.hpp
@@ -14,9 +14,54 @@ public:
     int run();
 
 private:
+    struct TestRun {
+        boost::filesystem::path inputFilePath;
+        boost::filesystem::path expectedOutputFilePath;
+    };
+
+    class TestBatch {
+    private:
+        boost::filesystem::path mDirectory_;
+        std::string pipeline_;
+        std::vector<TestRun> testRuns_;
+    public:
+        TestBatch(const boost::filesystem::path& mDirectory,
+                  const std::string& pipeline)
+            :mDirectory_(mDirectory), pipeline_(pipeline) {
+        }
+
+        boost::filesystem::path getDirectory() const {
+            return mDirectory_;
+        }
+
+        std::string getPipeline() const {
+            return pipeline_;
+        }
+
+        void addTestRun(const TestRun& testRun) {
+            testRuns_.push_back(testRun);
+        }
+
+
+    };
+
     static boost::program_options::options_description optionsHandled();
 
+    void lookForTestBatches_(const boost::filesystem::path& directory);
+    void lookInDirectory_(const boost::filesystem::path& directory);
+    void addTestBatch_(const boost::filesystem::path& directory);
+    void runAllBatches_();
+    void runBatch_(const TestBatch& batch);
+
+    std::string readCommand_(boost::filesystem::path commandFileName);
+    std::string stripComments_(const std::string& line);
+
     std::vector<boost::filesystem::path> directories_;
+
+    std::vector<TestBatch> testBatches_;
+
+    const static boost::filesystem::path TEST_BATCH_DIRECTORY_NAME;
+    const static boost::filesystem::path TEST_COMMAND_FILE_NAME;
 };
 
 #endif
