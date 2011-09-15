6229909a44b00acae75c7a17fadd8cd732a1ad9f
Paweł Skórzewski <pawel.skorzewski@amu.edu.pl>
2011-09-15 10:13:44 +0200

    LatticeWriterOutputIterator class added

---
+++ common/lattice_writer_output_iterator.cpp
@@ -0,0 +1,25 @@
+#include "lattice_writer_output_iterator.hpp"
+
+void LatticeWriterOutputIterator::putElement(const std::string& element) {
+    doPutElement(element);
+}
+
+void LatticeWriterOutputIterator::openAlternative() {
+    doOpenAlternative();
+}
+
+void LatticeWriterOutputIterator::closeAlternative() {
+    doCloseAlternative();
+}
+
+void LatticeWriterOutputIterator::openGroup(const std::string& group) {
+    doOpenGroup(group);
+}
+
+void LatticeWriterOutputIterator::closeGroup(const std::string& group) {
+    doCloseGroup(group);
+}
+
+LatticeWriterOutputIterator::~LatticeWriterOutputIterator() {
+}
+
---
+++ common/lattice_writer_output_iterator.hpp
@@ -0,0 +1,54 @@
+#ifndef LATTICE_WRITER_OUTPUT_ITERATOR_HDR
+#define LATTICE_WRITER_OUTPUT_ITERATOR_HDR
+
+#include <string>
+
+#include "lattice.hpp"
+
+/**
+ * Lattice writers write their results using
+ * lattice writer output iterators.
+ */
+class LatticeWriterOutputIterator {
+public:
+    /**
+     * Puts an element into the iterator.
+     */
+    void putElement(const std::string& element);
+
+    /**
+     * Opens a group of alternative elements.
+     */
+    void openAlternative();
+
+    /**
+     * Closes a group of alternative elements.
+     */
+    void closeAlternative();
+
+    /**
+     * Opens a group.
+     */
+    void openGroup(const std::string& group);
+
+    /**
+     * Closes a group.
+     */
+    void closeGroup(const std::string& group);
+
+    virtual ~LatticeWriterOutputIterator();
+
+private:
+    virtual void doPutElement(const std::string& element) = 0;
+
+    virtual void doOpenAlternative() = 0;
+
+    virtual void doCloseAlternative() = 0;
+
+    virtual void doOpenGroup(const std::string& group) = 0;
+
+    virtual void doCloseGroup(const std::string& group) = 0;
+
+};
+
+#endif
