3162315cbeb13d3cbed73dd5f8b05d2a1e2f1e8e
Filip Gralinski <filipg@ceti.pl>
2011-09-15 09:45:04 +0200

    simple_writer added to psi-pipe

--- framework/main_factories_keeper.cpp
+++ framework/main_factories_keeper.cpp
@@ -4,6 +4,7 @@
 
 #include "txt_lattice_reader.hpp"
 #include "psi_lattice_writer.hpp"
+#include "simple_lattice_writer.hpp"
 #include "tp_tokenizer.hpp"
 #include "srx_segmenter.hpp"
 #include "lemmatizer_annotator.hpp"
@@ -20,6 +21,7 @@
 MainFactoriesKeeper::MainFactoriesKeeper() {
     keeper_.takeProcessorFactory(new TxtLatticeReader::Factory());
     keeper_.takeProcessorFactory(new PsiLatticeWriter::Factory());
+    keeper_.takeProcessorFactory(new SimpleLatticeWriter::Factory());
     keeper_.takeProcessorFactory(new TpTokenizer::Factory());
     keeper_.takeProcessorFactory(new SrxSegmenter::Factory());
 #if HAVE_POSTGRESQL
