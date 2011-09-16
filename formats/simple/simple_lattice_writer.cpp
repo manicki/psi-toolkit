3313578e5746cc5926681f3502af3769759d3dd0
Paweł Skórzewski <pawel.skorzewski@amu.edu.pl>
2011-09-16 10:16:29 +0200

    Simple writer's options

--- formats/simple/simple_lattice_writer.cpp
+++ formats/simple/simple_lattice_writer.cpp
@@ -6,19 +6,19 @@ std::string SimpleLatticeWriter::getFormatName() {
 }
 
 LatticeWriter* SimpleLatticeWriter::Factory::doCreateLatticeWriter(
-    const boost::program_options::variables_map& vm
+    const boost::program_options::variables_map& options
 ) {
     std::map<std::string, std::string> tagsSeparators;
-    if (vm.count("tag")) {
-        tagsSeparators[vm["tag"].as<std::string>()] = "\n";
+    if (options.count("tag")) {
+        tagsSeparators[options["tag"].as<std::string>()] = "\n";
     }
 
     return new SimpleLatticeWriter(
-        vm.count("linear"),
-        vm.count("no-alts"),
-        vm.count("tag") ? (vm["tag"].as<std::string>()) : "token",
-        vm.count("sep") ? (vm["sep"].as<std::string>()) : "\n",
-        vm.count("alt-sep") ? (vm["alt-sep"].as<std::string>()) : "|",
+        options.count("linear"),
+        options.count("no-alts"),
+        options.count("tag") ? (options["tag"].as<std::string>()) : "token",
+        options.count("sep") ? (options["sep"].as<std::string>()) : "\n",
+        options.count("alt-sep") ? (options["alt-sep"].as<std::string>()) : "|",
         tagsSeparators
     );
 }
@@ -27,11 +27,11 @@ boost::program_options::options_description SimpleLatticeWriter::Factory::doOpti
     boost::program_options::options_description optionsDescription("Allowed options");
 
     optionsDescription.add_options()
-        ("alt-sep", "alternative edges separator")
+        ("alt-sep", boost::program_options::value<std::string>(), "alternative edges separator")
         ("linear", "skips cross-edges")
         ("no-alts", "skips alternative edges")
-        ("sep", "basic tag separator")
-        ("tag", "basic tag")
+        ("sep", boost::program_options::value<std::string>(), "basic tag separator")
+        ("tag", boost::program_options::value<std::string>(), "basic tag")
         ;
 
     return optionsDescription;
