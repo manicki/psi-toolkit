6fce6dae43877faa5fae53af104a3cfcec11a487
Paweł Skórzewski <pawel.skorzewski@amu.edu.pl>
2011-09-16 08:42:59 +0200

    LatticeIterWriter connected to SimplelatticeWriter

--- formats/simple/simple_lattice_writer.cpp
+++ formats/simple/simple_lattice_writer.cpp
@@ -1,9 +1,5 @@
 #include "simple_lattice_writer.hpp"
 
-#include <iomanip>
-#include <iostream>
-
-#include "logging.hpp"
 
 std::string SimpleLatticeWriter::getFormatName() {
     return "simple";
@@ -67,9 +63,10 @@ void SimpleLatticeWriter::Worker::doRun() {
 
     std::vector<std::string> handledTags;
 
+    std::map<std::string, std::string> tagsSeparatorsMap = processor_.getTagsSeparatorsMap();
     for (
-        std::map<std::string, std::string>::iterator mi = processor_.getTagsSeparatorsMap().begin();
-        mi != processor_.getTagsSeparatorsMap().end();
+        std::map<std::string, std::string>::iterator mi = tagsSeparatorsMap.begin();
+        mi != tagsSeparatorsMap.end();
         ++mi
     ) {
         outputIterator.setSeparator((*mi).first, (*mi).second);
@@ -86,86 +83,8 @@ void SimpleLatticeWriter::Worker::doRun() {
 
     writer.run();
 
-/*
-    Lattice::VertexIterator vi(lattice_);
-    Lattice::VertexDescriptor vd = lattice_.getFirstVertex();
-    Lattice::EdgeDescriptor edge;
-
-    std::map<std::string, Lattice::VertexDescriptor> targets;
-    std::map<std::string, std::string> tagsSeparators;
-    for (
-        std::map<std::string, std::string>::iterator mi = tagsSeparators.begin();
-        mi != tagsSeparators.end();
-        ++mi
-    ) {
-        targets[(*mi).first] = vd;
-    }
-
-    std::stringstream allSs;
-    std::stringstream blockSs;
-
-    while (processor_.isLinear() ? true : vi.hasNext()) {
-        if (!processor_.isLinear()) {
-            vd = vi.next();
-        }
-        bool areEdgesToWrite = false;
-        std::stringstream vertexSs;
-        std::stringstream sepSs;
-        Lattice::InOutEdgesIterator oei
-            = lattice_.outEdges(vd, lattice_.getLayerTagManager().anyTag());
-        while(oei.hasNext()) {
-            edge = oei.next();
-            std::list<std::string> tags
-                = lattice_.getLayerTagManager().getTagNames(lattice_.getEdgeLayerTags(edge));
-            for (
-                std::list<std::string>::iterator ti = tags.begin();
-                ti != tags.end();
-                ++ti
-            ) {
-                if (processor_.getBasicTag() == *ti) {
-                    areEdgesToWrite = true;
-                    if (vertexSs.str() != "") {
-                        vertexSs << processor_.getAltSeparator();
-                    }
-                    if (lattice_.getAnnotationText(edge) == "") {
-                        vertexSs << lattice_.getAnnotationCategory(edge);
-                    } else {
-                        vertexSs << lattice_.getAnnotationText(edge);
-                    }
-                }
-                if (processor_.isHandledTag(*ti) && targets[*ti] == vd) {
-                    areEdgesToWrite = true;
-                    targets[*ti] = lattice_.getEdgeTarget(edge);
-                    sepSs << processor_.getTagSeparator(*ti);
-                }
-            }
-        }
-
-        if (sepSs.str() != "") {
-            allSs << blockSs.str();
-            if (allSs.str() != "") {
-                allSs << sepSs.str();
-            }
-            blockSs.str("");
-        }
-        if (blockSs.str() != "" && areEdgesToWrite) {
-            blockSs << processor_.getBasicTagSeparator();
-        }
-        blockSs << vertexSs.str();
-        if (processor_.isLinear()) {
-            try {
-                vd = lattice_.getEdgeTarget(
-                    lattice_.firstOutEdge(vd, lattice_.getLayerTagManager().anyTag())
-                );
-            } catch (NoEdgeException) {
-                break;
-            }
-        }
-    }
-    alignOutput_(allSs.str());
-    alignOutput_(blockSs.str());
     alignOutputNewline_();
-*/
+
 }
 
 SimpleLatticeWriter::Worker::~Worker() {
