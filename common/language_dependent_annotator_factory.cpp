8e82a4ea27eda3d3fcbe4df52c8011ecc3c5e29b
Filip Graliński <filipg@amu.edu.pl>
2011-09-17 12:11:59 +0200

    LanguageDependentAnnotatorFactory added

---
+++ common/language_dependent_annotator_factory.cpp
@@ -0,0 +1,19 @@
+#include "language_dependent_annotator_factory.hpp"
+
+LanguageDependentAnnotatorFactory::~LanguageDependentAnnotatorFactory() {
+}
+
+boost::program_options::options_description LanguageDependentAnnotatorFactory::doOptionsHandled() {
+    boost::program_options::options_description optionsDescription("Allowed options");
+
+    optionsDescription.add_options()
+        ("lang", boost::program_options::value<std::string>(), "language")
+        ("force-language", boost::program_options::value<bool>()->default_value(false)->zero_tokens(),
+         "force using specified language even if a text was resognised otherwise")
+        ;
+
+    doAddLanguageIndependentOptionsHandled(optionsDescription);
+
+    return optionsDescription;
+
+}
---
+++ common/language_dependent_annotator_factory.hpp
@@ -0,0 +1,28 @@
+#ifndef LANGUAGE_DEPENDENT_ANNOTATOR_FACTORY
+#define LANGUAGE_DEPENDENT_ANNOTATOR_FACTORY
+
+#include <boost/program_options/options_description.hpp>
+
+#include "annotator_factory.hpp"
+
+/*!
+  Factories for processing whose behaviour depends on the language of the text
+  should be derived from <this class>. This way options such `--lang` are
+  provided.
+*/
+
+class LanguageDependentAnnotatorFactory {
+
+public:
+    virtual ~LanguageDependentAnnotatorFactory();
+
+private:
+
+    virtual boost::program_options::options_description doOptionsHandled();
+
+    virtual void doAddLanguageIndependentOptionsHandled(
+        boost::program_options::options_description& optionsDescription) = 0;
+
+};
+
+#endif
