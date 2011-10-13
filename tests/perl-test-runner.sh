0310a3a9051a7523b607b2b02762364ef617bfce
Marcin Walas <marcinw@uam-5048.(none)>
2011-10-13 13:36:45 +0200

    first test for perl binding (load module + simple tokenization)

---
+++ tests/perl-test-runner.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PERL_BINDINGS_DIR=`pwd`/bindings/perl
+PRJ_DIR=`pwd`/prj
+
+if [[ -e $PRJ_DIR/libSimple.so ]]; then
+
+PATH=.:${PRJ_DIR}:${PATH}
+PERL5LIB=${PRJ_DIR}:${PERL5LIB}
+
+cd $PRJ_DIR
+perl $PERL_BINDINGS_DIR/Test.PL $PERL_BINDINGS_DIR
+
+else
+    echo 'Perl bindings NOT CREATED. Testing perl bindings ignored!'
+fi
