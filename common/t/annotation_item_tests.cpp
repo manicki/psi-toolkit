040723dc3a22ca654e90ad9de4dbd463934cc2bf
Paweł Skórzewski <pawels@uam5050>
2011-10-13 13:58:54 +0200

    Annotation item test added

--- common/t/annotation_item_tests.cpp
+++ common/t/annotation_item_tests.cpp
@@ -23,3 +23,14 @@ BOOST_AUTO_TEST_CASE( annotation_simple ) {
     BOOST_CHECK_EQUAL((*avi).first, "number");
     BOOST_CHECK_EQUAL((*avi).second, "plural");
 };
+
+BOOST_AUTO_TEST_CASE( annotation_compare ) {
+    AnnotationItemManager annotationItemManager;
+    AnnotationItem ai1("noun");
+    AnnotationItem ai2("noun");
+    annotationItemManager.setValue(ai1, "case", "nominative");
+    annotationItemManager.setValue(ai1, "number", "plural");
+    annotationItemManager.setValue(ai2, "number", "plural");
+    annotationItemManager.setValue(ai2, "case", "nominative");
+    BOOST_CHECK(ai1 == ai2);
+};
