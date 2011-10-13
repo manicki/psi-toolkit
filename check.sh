e1023836c72cfc9f4ff07e15b9ced77f9c251251
Filip Gralinski <filipg@ceti.pl>
2011-10-13 16:10:23 +0200

    excluding bindings/perl from cppcheck

--- check.sh
+++ check.sh
@@ -21,7 +21,7 @@ valgrind --xml=yes --xml-file=valgrind.xml --suppressions=./memcpy.supp ./tests/
 xsltproc  ~/valgrind-reports-to-xunit/xslt/valgrind_transform.xsl valgrind.xml > valgrindTestResults.xml
 
 cd ..
-cppcheck -D__cplusplus -f --xml . --enable=all echo `find . -type d ! -path './.git*' ! -path './prj*' | perl -ne 'chomp; print "-I$_ "'`  2> cppcheck-result.xml
+cppcheck -D__cplusplus -f --xml . --enable=all echo `find . -type d ! -path './.git*' ! -path './prj*' | perl -ne 'chomp; print "-I$_ "'` -i prj/bindings/perl 2> cppcheck-result.xml
 
 if ! type -P jruby > /dev/null; then
     echo -e "WARNING: jRuby has been not found!\n\n"
