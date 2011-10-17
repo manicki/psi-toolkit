d87c7247c5cac683b897b38ef8ef01828c24b47d
Roman Grundkiewicz <romang@amu.edu.pl>
2011-10-17 15:51:01 +0200

    Simple js validations added

---
+++ server/website/js/validations.js
@@ -0,0 +1,50 @@
+
+$(document).ready(function()
+{
+    var messages = [];
+
+    function clearMessages() {
+        messages = [];
+        $("#message").empty();
+    };
+
+    function addMessage(text) {
+        messages.push("<li>" + text + "</li>");
+    };
+
+    function showMessage() {
+        if (messages.length) {
+            var mess = $("<ul>").html(messages.join(''));
+            $("#message").html("<p>Some errors occured:</p>").append(mess);
+            return true;
+        }
+        return false;
+    };
+
+    $("form").submit(function() {
+        clearMessages();
+
+        if (!$("#input-pipe").val()) {
+            addMessage("Pipe specification cannot be empty.");
+        }
+
+        if ($("#radio-input-text").attr("checked") == 'checked') {
+            if (!$("#input-text textarea").val()) {
+                addMessage("Write some text to process.");
+            }
+        }
+        else {
+            if (!$("#input-file").val()) {
+                addMessage("Select file with text to process.");
+            }
+        }
+
+        if (showMessage()) {
+            return false;
+        }
+        $(this).submit();
+    });
+
+
+});
+
