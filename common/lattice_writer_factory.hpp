e0f03b987db857a01f4d289e77482e2aae288733
Filip Graliński <filipg@amu.edu.pl>
2011-10-20 21:37:36 +0200

    LatticeWriterFactory in only header file

--- common/lattice_writer_factory.hpp
+++ common/lattice_writer_factory.hpp
@@ -10,18 +10,25 @@ public:
     /**
      * Creates a lattice writer with the options specified.
      */
-    LatticeWriter* createLatticeWriter(const boost::program_options::variables_map& options);
+    LatticeWriter* createLatticeWriter(const boost::program_options::variables_map& options) {
+        return dynamic_cast<LatticeWriter*>(createProcessor(options));
+    }
 
-    virtual ~LatticeWriterFactory();
+    virtual ~LatticeWriterFactory() {
+    }
 
 private:
 
-    virtual LatticeWriter* doCreateProcessor(const boost::program_options::variables_map& options);
+    virtual LatticeWriter* doCreateProcessor(const boost::program_options::variables_map& options) {
+        return doCreateLatticeWriter(options);
+    }
 
     virtual LatticeWriter* doCreateLatticeWriter(const boost::program_options::variables_map& options) = 0;
 
     std::string doGetContinuation(
-        const boost::program_options::variables_map& options) const;
+        const boost::program_options::variables_map& /*options*/) const {
+        return std::string();
+    }
 
 };
 
