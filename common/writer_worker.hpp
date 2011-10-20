15e68b4138b884d0dfffb9a39bd5e60a5a7d6c56
Filip Graliński <filipg@amu.edu.pl>
2011-10-20 22:03:04 +0200

    WriterWorker only in a header file

--- common/writer_worker.hpp
+++ common/writer_worker.hpp
@@ -8,29 +8,47 @@
 class WriterWorker : public LatticeWorker {
 
 public:
-    WriterWorker(std::ostream& outputStream, Lattice& lattice);
+    WriterWorker(std::ostream& outputStream, Lattice& lattice)
+        :LatticeWorker(lattice), outputStream_(outputStream), currentPos_(0){
+    }
 
-    virtual ~WriterWorker();
+    virtual ~WriterWorker() {
+    }
 
 protected:
     /**
      * Prints output to outputStream_ and updates cursor position.
      * Returns the new cursor position.
      */
-    unsigned int alignOutput_(std::string output);
+    unsigned int alignOutput_(std::string output) {
+        outputStream_ << output;
+        currentPos_ += utf8::distance(output.begin(), output.end());
+        return currentPos_;
+    }
 
     /**
      * Prints output to outputStream_ and aligns cursor to the required position (pos) if possible.
      * Strings are aligned left.
      * Returns the real new cursor position.
      */
-    unsigned int alignOutput_(std::string output, unsigned int pos, char padChar = ' ');
+    unsigned int alignOutput_(std::string output, unsigned int pos, char padChar = ' ') {
+        outputStream_ << output;
+        currentPos_ += utf8::distance(output.begin(), output.end());
+        for (; currentPos_ < pos; ++currentPos_) {
+            outputStream_ << padChar;
+        }
+        return currentPos_;
+    }
 
     /**
      * Prints newline to outputStream_ and resets cursor position.
      * Returns the new cursor position (zero).
      */
-    unsigned int alignOutputNewline_();
+    unsigned int alignOutputNewline_() {
+        outputStream_ << std::endl;
+        currentPos_ = 0;
+        return currentPos_;
+    }
 
     std::ostream& getOutputStream() { return outputStream_; }
 
