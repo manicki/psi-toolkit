1b99b1c6379babcb16f1c11b969dc9ab3713429f
Filip Gralinski <filipg@ceti.pl>
2011-10-21 11:03:51 +0200

    all LatticeReaderFactory in a header file

--- common/lattice_reader_factory.hpp
+++ common/lattice_reader_factory.hpp
@@ -10,16 +10,24 @@ public:
     /**
      * Creates a lattice reader with the options specified.
      */
-    LatticeReader* createLatticeReader(const boost::program_options::variables_map& options);
+    LatticeReader* createLatticeReader(const boost::program_options::variables_map& options) {
+        return dynamic_cast<LatticeReader*>(createProcessor(options));
+    }
 
 private:
 
-    virtual LatticeReader* doCreateProcessor(const boost::program_options::variables_map& options);
+    virtual LatticeReader* doCreateProcessor(
+        const boost::program_options::variables_map& options) {
+        return doCreateLatticeReader(options);
+    }
 
-    virtual LatticeReader* doCreateLatticeReader(const boost::program_options::variables_map& options) = 0;
+    virtual LatticeReader* doCreateLatticeReader(
+        const boost::program_options::variables_map& options) = 0;
 
     virtual std::string doGetContinuation(
-        const boost::program_options::variables_map& options) const;
+        const boost::program_options::variables_map& /*options*/) const {
+        return std::string("psi-writer");
+    }
 
 };
 
