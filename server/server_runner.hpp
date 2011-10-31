943df6144cd1b9ce25f42b7008b7b879061f0458
Filip Gralinski <filipg@ceti.pl>
2011-10-31 12:31:09 +0100

    --daemon option added

--- server/server_runner.hpp
+++ server/server_runner.hpp
@@ -2,6 +2,7 @@
 #define SERVER_RUNNER_HDR
 
 #include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
 #include <sstream>
 
 #include "index_site.hpp"
@@ -24,6 +25,11 @@ private:
     std::string annotatorOptionsAsString();
 
     int executeOptions();
+
+    void daemonize_(bool leaveStandardDescriptors);
+    int setRootDirectory_();
+
+    boost::filesystem::path rootDir_;
 
     static const std::string DEFAULT_PIPE;
 };
