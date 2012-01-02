04a6054903cb4dcad9378c89f3c899b3a681f140
Roman Grundkiewicz <romang@amu.edu.pl>
2012-01-02 09:07:54 +0100

    last style bugs fixed

--- server/server/server.cpp
+++ server/server/server.cpp
@@ -1,5 +1,6 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at <a href="http://www.boost.org/LICENSE_1_0.txt">http://www.boost.org/LICENSE_1_0.txt)
+// file LICENSE_1_0.txt or copy at <a href="http://www.boost.org/LICENSE_1_0.txt">
+// http://www.boost.org/LICENSE_1_0.txt)
 //
 
 #include "server.hpp"
@@ -53,7 +54,7 @@ void server::run()
   {
     threads[i]->join();
   }
-  
+
   INFO("Stopped!");
 }
 
