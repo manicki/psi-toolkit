6fab6b7c813f4b50597a0a88242c1250ce99bc43
Roman Grundkiewicz <romang@amu.edu.pl>
2012-01-13 11:56:36 +0100

    Adding host and port arguments to server/t/runner.rb script

--- server/t/config.rb
+++ server/t/config.rb
@@ -1,10 +1,9 @@
-hostname = '0.0.0.0'
-port = '3000'
+$host ||= '0.0.0.0'
+$port ||= '3000'
 threads = '1'
 current_dir = File.dirname(__FILE__)
 website = File.expand_path(current_dir + '/../website/')
 
 $source_dir = File.expand_path(current_dir + '/../../build/')
-$server_address = "http://#{hostname}:#{port}/"
-$server_localhost = "http://localhost:#{port}/"
-$arguments = "--address #{hostname} --port #{port} --threads #{threads} --root #{website}"
+$server_address = "http://#{$host}:#{$port}/"
+$arguments = "--address #{$host} --port #{$port} --threads #{threads} --root #{website}"
--- server/t/psi_server_test.rb
+++ server/t/psi_server_test.rb
@@ -9,7 +9,7 @@ class PsiServerTest < Test::Unit::TestCase
 	def test_server_address
 		assert_nothing_raised(NativeException) do
 			browser = Celerity::Browser.new
-			browser.goto $server_localhost
+			browser.goto $server_address
 			browser.close
 		end
 	end
