443469f087e6a4b23c2e6c77a49a2b20e90d5d1f
Paweł Skórzewski <pawels@uam5050>
2012-01-30 10:14:40 +0100

    UTT reader's tests moved from common/t to formats/utt/t

--- common/t/lattice_readers_writers_tests.cpp
+++ common/t/lattice_readers_writers_tests.cpp
@@ -9,120 +9,14 @@
 #include "psi_lattice_writer.hpp"
 #include "simple_lattice_writer.hpp"
 #include "simple_lattice_writer_stream_output_iterator.hpp"
-#include "utt_lattice_reader.hpp"
 
 #include "config.hpp"
 
 void prepareSimpleLattice_(Lattice & lattice);
 void prepareAdvancedLattice_(Lattice & lattice);
 
-BOOST_AUTO_TEST_SUITE( utt_lattice_reader )
+BOOST_AUTO_TEST_SUITE( lattice_readers_writers )
 
-BOOST_AUTO_TEST_CASE( utt_lattice_reader ) {
-    Lattice lattice("");
-
-    boost::scoped_ptr<StreamLatticeReader> reader(new UTTLatticeReader());
-
-  reader->readIntoLattice("../formats/utt/t/files/fr_simple_puddle_input.txt", lattice);
-
-  LayerTagMask rawMask = lattice.getLayerTagManager().getMask("symbol");
-  Lattice::EdgesSortedBySourceIterator rei = lattice.edgesSortedBySource(rawMask);
-
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'A");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'m");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'e");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'l");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'i");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'e");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "' ");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'a");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "' ");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'u");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'n");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "' ");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'c");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'h");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'a");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'t");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'.");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'Q");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'u");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'e");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'l");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'l");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'e");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "' ");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'â");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'g");
-  BOOST_CHECK(rei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(rei.next()), "'e");
-
-  LayerTagMask tokenMask = lattice.getLayerTagManager().getMask("token");
-  Lattice::EdgesSortedBySourceIterator tei = lattice.edgesSortedBySource(tokenMask);
-
-  BOOST_CHECK(tei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(tei.next()), "'Amelie'");
-  BOOST_CHECK(tei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(tei.next()), "' '");
-  BOOST_CHECK(tei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(tei.next()), "'a'");
-  BOOST_CHECK(tei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(tei.next()), "' '");
-  BOOST_CHECK(tei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(tei.next()), "'un'");
-  BOOST_CHECK(tei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(tei.next()), "' '");
-  BOOST_CHECK(tei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(tei.next()), "'chat'");
-  BOOST_CHECK(tei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(tei.next()), "'.'");
-  BOOST_CHECK(tei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(tei.next()), "'Quelle'");
-  BOOST_CHECK(tei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(tei.next()), "' '");
-  BOOST_CHECK(tei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationCategory(tei.next()), "'âge'");
-
-  LayerTagMask sentenceMask = lattice.getLayerTagManager().getMask("sentence");
-  Lattice::EdgesSortedBySourceIterator sei = lattice.edgesSortedBySource(sentenceMask);
-
-  BOOST_CHECK(sei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationText(sei.next()),
-  "Amelie a un chat.");
-  BOOST_CHECK(sei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationText(sei.next()),
-  "Quelle âge as-tu?");
-  BOOST_CHECK(sei.hasNext());
-  BOOST_CHECK_EQUAL(lattice.getAnnotationText(sei.next()),
-  "Karol Józef Wojtyła est élu pape de l'Église catholique romaine le 16 octobre 1978.");
-  BOOST_CHECK(!sei.hasNext());
-}
 
 BOOST_AUTO_TEST_CASE( psi_lattice_writer_simple ) {
     Lattice lattice;
