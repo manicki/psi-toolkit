7d9ab8f2a499ef03dd8f845d9e717bd241ed46fe
Paweł Skórzewski <pawels@uam5050>
2012-01-30 14:11:19 +0100

    Advanced lattice iter writer's test added

--- formats/simple/t/lattice_iter_writer_tests.cpp
+++ formats/simple/t/lattice_iter_writer_tests.cpp
@@ -60,4 +60,53 @@ BOOST_AUTO_TEST_CASE( lattice_iter_writer ) {
 }
 
 
+BOOST_AUTO_TEST_CASE( lattice_iter_writer_advanced ) {
+
+    Lattice lattice;
+    writers_tests_utils::prepareRegularLattice(lattice);
+
+    std::vector<std::string> handledTags;
+    handledTags.push_back("level1");
+    handledTags.push_back("level2");
+
+    std::ostringstream osstr;
+
+    SimpleLatticeWriterStreamOutputIterator outputIterator(
+        std::cout,// osstr,
+        "|",
+        ","
+    );
+
+    outputIterator.setSeparator("level1", ";");
+    outputIterator.setSeparator("level2", "\n");
+
+    boost::scoped_ptr<LatticeIterWriter> writer(new LatticeIterWriter(
+        lattice,
+        outputIterator,
+        false, //linear
+        false, //no-alts
+        true, //with-blank
+        "token", //basicTag
+        handledTags
+    ));
+
+    writer->run();
+
+    std::cout << std::endl;
+
+    osstr << std::endl;
+
+    std::string line;
+    std::string contents;
+    std::ifstream s(ROOT_DIR "formats/simple/t/files/regular.txt");
+    while (getline(s, line)) {
+        contents += line;
+        contents += "\n";
+    }
+
+    BOOST_CHECK_EQUAL(osstr.str(), contents);
+
+}
+
+
 BOOST_AUTO_TEST_SUITE_END()
