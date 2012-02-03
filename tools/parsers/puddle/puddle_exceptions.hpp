d4209001434101ce830d10d51243fb5e5d174ccc
Paweł Skórzewski <pawels@uam5050>
2012-02-03 13:13:28 +0100

    Psi exceptions inheritance changed

--- tools/parsers/puddle/puddle_exceptions.hpp
+++ tools/parsers/puddle/puddle_exceptions.hpp
@@ -1,60 +1,60 @@
 #ifndef PUDDLE_EXCEPTIONS_HDR
 #define PUDDLE_EXCEPTIONS_HDR
 
-#include "exceptions.hpp"
+#include "psi_exception.hpp"
 
-class PuddleRuleElementNotMatchedException : public Exception {
+class PuddleRuleElementNotMatchedException : public PsiException {
     public:
         PuddleRuleElementNotMatchedException(std::string message) :
-            Exception(message) { }
+            PsiException(message) { }
 };
 
-class PuddleNoLemmaEdgeException : public Exception {
+class PuddleNoLemmaEdgeException : public PsiException {
     public:
         PuddleNoLemmaEdgeException(std::string message) :
-            Exception(message) { }
+            PsiException(message) { }
 };
 
-class PuddleNoLexemeEdgeException : public Exception {
+class PuddleNoLexemeEdgeException : public PsiException {
     public:
         PuddleNoLexemeEdgeException(std::string message) :
-            Exception(message) { }
+            PsiException(message) { }
 };
 
-class PuddleNoMatchingEdgeException : public Exception {
+class PuddleNoMatchingEdgeException : public PsiException {
     public:
         PuddleNoMatchingEdgeException(std::string message) :
-            Exception(message) { }
+            PsiException(message) { }
 };
 
-class PuddleNoTagsetException : public Exception {
+class PuddleNoTagsetException : public PsiException {
     public:
         PuddleNoTagsetException(std::string message) :
-            Exception(message) { }
+            PsiException(message) { }
 };
 
-class PuddleNoRulesException : public Exception {
+class PuddleNoRulesException : public PsiException {
     public:
         PuddleNoRulesException(std::string message) :
-            Exception(message) { }
+            PsiException(message) { }
 };
 
-class PuddleRuleSyntaxException : public Exception {
+class PuddleRuleSyntaxException : public PsiException {
     public:
         PuddleRuleSyntaxException(std::string message) :
-            Exception(message) { }
+            PsiException(message) { }
 };
 
-class PuddleTagsetSyntaxException : public Exception {
+class PuddleTagsetSyntaxException : public PsiException {
     public:
         PuddleTagsetSyntaxException(std::string message) :
-            Exception(message) { }
+            PsiException(message) { }
 };
 
-class PuddleIOException : public Exception {
+class PuddleIOException : public PsiException {
     public:
         PuddleIOException(std::string message) :
-            Exception(message) { }
+            PsiException(message) { }
 };
 
 
