6c5dac0fdbe6f753fc1506290b33a3c5b49cc3b3
Paweł Skórzewski <pawels@uam5050>
2012-02-06 09:46:07 +0100

    Small corrections in parser tests

--- tools/parsers/gobio/t/parser_tests.cpp
+++ tools/parsers/gobio/t/parser_tests.cpp
@@ -22,20 +22,20 @@ BOOST_AUTO_TEST_CASE( combinator ) {
     simple_cfg_combinator<char, simple_cfg_rule<char> > combinator;
 
     combinator.add_unary_rule('X', simple_cfg_rule<char>('S', 1.0));
-    combinator.add_binary_rule('A', 'Y', simple_cfg_rule<char>('X', 1.0));
+    combinator.add_binary_rule('a', 'Y', simple_cfg_rule<char>('X', 1.0));
     combinator.add_binary_rule('c', 'Y', simple_cfg_rule<char>('X', 1.0));
     combinator.add_binary_rule('c', 'Y', simple_cfg_rule<char>('S', 1.0));
-    combinator.add_binary_rule('X', 'B', simple_cfg_rule<char>('Y', 1.0));
+    combinator.add_binary_rule('X', 'b', simple_cfg_rule<char>('Y', 1.0));
     combinator.add_binary_rule('X', 'd', simple_cfg_rule<char>('Y', 1.0));
-    combinator.add_unary_rule('B', simple_cfg_rule<char>('Y', 1.0));
+    combinator.add_unary_rule('b', simple_cfg_rule<char>('Y', 1.0));
     combinator.add_binary_rule('c', 'Y', simple_cfg_rule<char>('S', 2.0));
-    combinator.add_unary_rule('B', simple_cfg_rule<char>('T', 1.0));
+    combinator.add_unary_rule('b', simple_cfg_rule<char>('T', 1.0));
 
-    BOOST_CHECK_EQUAL(combinator.combine_unary('B').size(), 2);
+    BOOST_CHECK_EQUAL(combinator.combine_unary('b').size(), 2);
     BOOST_CHECK_EQUAL(combinator.combine_unary('d').size(), 0);
-    BOOST_CHECK_EQUAL(combinator.combine_binary('A', 'Y').size(), 1);
+    BOOST_CHECK_EQUAL(combinator.combine_binary('a', 'Y').size(), 1);
     BOOST_CHECK_EQUAL(combinator.combine_binary('c', 'Y').size(), 3);
-    BOOST_CHECK_EQUAL(combinator.combine_binary('B', 'Y').size(), 0);
+    BOOST_CHECK_EQUAL(combinator.combine_binary('b', 'Y').size(), 0);
 
 }
 
@@ -73,7 +73,7 @@ BOOST_AUTO_TEST_CASE( helpers ) {
     BOOST_CHECK(is_consistent(ch));
 
     ADD_UNARY_RULE("B", "b");
-    ADD_UNARY_RULE("A", "A");
+    ADD_UNARY_RULE("A", "a");
     ADD_BINARY_RULE("X", "B", "A");
     ADD_BINARY_RULE("X", "X", "A");
     ADD_UNARY_RULE("S", "X");
