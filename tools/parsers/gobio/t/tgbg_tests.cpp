506c8a4a70b44bf07d931b3dc4055fa222fde793
Paweł Skórzewski <pawels@uam5050>
2012-02-20 15:27:44 +0100

    Tgbg's test reading Markov binarized rules

--- tools/parsers/gobio/t/tgbg_tests.cpp
+++ tools/parsers/gobio/t/tgbg_tests.cpp
@@ -15,8 +15,42 @@ BOOST_AUTO_TEST_CASE( reading_rules ) {
         number_master,
         semantics_stub<int, number_master, double>
     > tgbg;
-
     tgbg.add_rules(ROOT_DIR "tools/parsers/gobio/t/files/rules_3.g");
+
+}
+
+
+BOOST_AUTO_TEST_CASE( reading_markov_binarized_rules ) {
+
+    {
+        tgbg_combinator<
+            int,
+            Lattice::Score,
+            number_master,
+            semantics_stub<int, number_master, double>
+        > tgbg;
+        tgbg.add_rules(ROOT_DIR "tools/parsers/gobio/t/files/rules_4.g");
+    }
+
+    {
+        tgbg_combinator<
+            int,
+            Lattice::Score,
+            number_master,
+            semantics_stub<int, number_master, double>
+        > tgbg;
+        tgbg.add_rules(ROOT_DIR "tools/parsers/gobio/t/files/rules_5.g");
+    }
+
+    {
+        tgbg_combinator<
+            int,
+            Lattice::Score,
+            number_master,
+            semantics_stub<int, number_master, double>
+        > tgbg;
+        tgbg.add_rules(ROOT_DIR "tools/parsers/gobio/t/files/rules_6.g");
+    }
 
 }
 
