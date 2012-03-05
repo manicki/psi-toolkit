c855f0012bcb01102b927d6f51b28fb570d79efa
Paweł Skórzewski <pawels@uam5050>
2012-03-05 14:09:56 +0100

    GV-writer's tests fixed

--- formats/gv/t/gv_writer_tests.cpp
+++ formats/gv/t/gv_writer_tests.cpp
@@ -4,6 +4,7 @@
 #include <set>
 #include <string>
 
+#include <boost/algorithm/string/trim.hpp>
 #include <boost/scoped_ptr.hpp>
 
 #include "lattice_preparators.hpp"
@@ -30,18 +31,22 @@ BOOST_AUTO_TEST_CASE( gv_lattice_writer_simple ) {
         false // tree
     ));
 
-    std::ostringstream osstr;
-    writer->writeLattice(lattice, osstr);
-
-    std::string line;
-    std::string contents;
-    std::ifstream s(ROOT_DIR "formats/gv/t/files/simple.dot");
-    while (getline(s, line)) {
-        contents += line;
-        contents += "\n";
+    std::stringstream outSs;
+    writer->writeLattice(lattice, outSs);
+    std::string refLine;
+    std::string outLine;
+    std::ifstream reference(ROOT_DIR "formats/gv/t/files/simple.dot");
+    while (getline(reference, refLine)) {
+        if (refLine.find("pos") == std::string::npos) continue;
+        BOOST_CHECK(getline(outSs, outLine));
+        while (outLine.find("pos") == std::string::npos) {
+            BOOST_CHECK(getline(outSs, outLine));
+        }
+        BOOST_CHECK_EQUAL(
+            boost::algorithm::trim_copy(refLine.substr(0, refLine.find("pos"))),
+            boost::algorithm::trim_copy(outLine.substr(0, outLine.find("pos")))
+        );
     }
-
-    BOOST_CHECK_EQUAL(osstr.str(), contents);
 
 }
 
@@ -63,18 +68,22 @@ BOOST_AUTO_TEST_CASE( gv_lattice_writer_advanced ) {
         false // tree
     ));
 
-    std::ostringstream osstr;
-    writer->writeLattice(lattice, osstr);
-
-    std::string line;
-    std::string contents;
-    std::ifstream s(ROOT_DIR "formats/gv/t/files/advanced.dot");
-    while (getline(s, line)) {
-        contents += line;
-        contents += "\n";
+    std::stringstream outSs;
+    writer->writeLattice(lattice, outSs);
+    std::string refLine;
+    std::string outLine;
+    std::ifstream reference(ROOT_DIR "formats/gv/t/files/advanced.dot");
+    while (getline(reference, refLine)) {
+        if (refLine.find("pos") == std::string::npos) continue;
+        BOOST_CHECK(getline(outSs, outLine));
+        while (outLine.find("pos") == std::string::npos) {
+            BOOST_CHECK(getline(outSs, outLine));
+        }
+        BOOST_CHECK_EQUAL(
+            boost::algorithm::trim_copy(refLine.substr(0, refLine.find("pos"))),
+            boost::algorithm::trim_copy(outLine.substr(0, outLine.find("pos")))
+        );
     }
-
-    BOOST_CHECK_EQUAL(osstr.str(), contents);
 
 }
 
