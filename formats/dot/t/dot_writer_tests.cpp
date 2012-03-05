8e453aca9ebdc4f68cfd32e8675b6f4d080e285e
Paweł Skórzewski <pawels@uam5050>
2012-03-05 12:19:49 +0100

    DOT/GV writers' tree tests added

--- formats/dot/t/dot_writer_tests.cpp
+++ formats/dot/t/dot_writer_tests.cpp
@@ -73,4 +73,34 @@ BOOST_AUTO_TEST_CASE( dot_lattice_writer_advanced ) {
 }
 
 
+BOOST_AUTO_TEST_CASE( dot_lattice_writer_tree ) {
+
+    Lattice lattice;
+    lattice_preparators::prepareRegularLattice(lattice);
+
+    std::set<std::string> filter;
+
+    boost::scoped_ptr<LatticeWriter<std::ostream> > writer(new DotLatticeWriter(
+        false, // show tags
+        false, // color
+        filter, // filter
+        true // tree
+    ));
+
+    std::ostringstream osstr;
+    writer->writeLattice(lattice, osstr);
+
+    std::string line;
+    std::string contents;
+    std::ifstream s(ROOT_DIR "formats/dot/t/files/tree.dot");
+    while (getline(s, line)) {
+        contents += line;
+        contents += "\n";
+    }
+
+    BOOST_CHECK_EQUAL(osstr.str(), contents);
+
+}
+
+
 BOOST_AUTO_TEST_SUITE_END()
