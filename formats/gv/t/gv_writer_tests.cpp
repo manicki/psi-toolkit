7a8d7423f2adf0c140bf371014526748c2ae45b0
Paweł Skórzewski <pawels@uam5050>
2012-03-05 14:20:35 +0100

    GV-writer's tree test added

--- formats/gv/t/gv_writer_tests.cpp
+++ formats/gv/t/gv_writer_tests.cpp
@@ -36,6 +36,7 @@ BOOST_AUTO_TEST_CASE( gv_lattice_writer_simple ) {
     std::string refLine;
     std::string outLine;
     std::ifstream reference(ROOT_DIR "formats/gv/t/files/simple.dot");
+    int linesCount = 0;
     while (getline(reference, refLine)) {
         if (refLine.find("pos") == std::string::npos) continue;
         BOOST_CHECK(getline(outSs, outLine));
@@ -46,7 +47,9 @@ BOOST_AUTO_TEST_CASE( gv_lattice_writer_simple ) {
             boost::algorithm::trim_copy(refLine.substr(0, refLine.find("pos"))),
             boost::algorithm::trim_copy(outLine.substr(0, outLine.find("pos")))
         );
+        ++linesCount;
     }
+    BOOST_CHECK_EQUAL(linesCount, 32);
 
 }
 
@@ -73,6 +76,7 @@ BOOST_AUTO_TEST_CASE( gv_lattice_writer_advanced ) {
     std::string refLine;
     std::string outLine;
     std::ifstream reference(ROOT_DIR "formats/gv/t/files/advanced.dot");
+    int linesCount = 0;
     while (getline(reference, refLine)) {
         if (refLine.find("pos") == std::string::npos) continue;
         BOOST_CHECK(getline(outSs, outLine));
@@ -83,7 +87,49 @@ BOOST_AUTO_TEST_CASE( gv_lattice_writer_advanced ) {
             boost::algorithm::trim_copy(refLine.substr(0, refLine.find("pos"))),
             boost::algorithm::trim_copy(outLine.substr(0, outLine.find("pos")))
         );
+        ++linesCount;
     }
+    BOOST_CHECK_EQUAL(linesCount, 45);
+
+}
+
+
+BOOST_AUTO_TEST_CASE( dot_lattice_writer_tree ) {
+
+    Lattice lattice;
+    lattice_preparators::prepareRegularLattice(lattice);
+
+    std::set<std::string> filter;
+
+    boost::scoped_ptr<LatticeWriter<std::ostream> > writer(new GVLatticeWriter(
+        false, // show tags
+        false, // color
+        filter, // filter
+        "dot", // output format
+        "", // output file name
+        true, // use outputStream
+        true // tree
+    ));
+
+    std::stringstream outSs;
+    writer->writeLattice(lattice, outSs);
+    std::string refLine;
+    std::string outLine;
+    std::ifstream reference(ROOT_DIR "formats/gv/t/files/tree.dot");
+    int linesCount = 0;
+    while (getline(reference, refLine)) {
+        if (refLine.find("pos") == std::string::npos) continue;
+        BOOST_CHECK(getline(outSs, outLine));
+        while (outLine.find("pos") == std::string::npos) {
+            BOOST_CHECK(getline(outSs, outLine));
+        }
+        BOOST_CHECK_EQUAL(
+            boost::algorithm::trim_copy(refLine.substr(0, refLine.find("pos"))),
+            boost::algorithm::trim_copy(outLine.substr(0, outLine.find("pos")))
+        );
+        ++linesCount;
+    }
+    BOOST_CHECK_EQUAL(linesCount, 237);
 
 }
 
