835d3fbef13a79adac5b89c879e2dc7889ee0db6
Paweł Skórzewski <pawels@uam5050>
2012-03-21 14:50:07 +0100

    NKJP reader handles token partitions

--- formats/nkjp/nkjp_lattice_reader.cpp
+++ formats/nkjp/nkjp_lattice_reader.cpp
@@ -62,7 +62,7 @@ NKJPLatticeReader::Worker::Worker(
 { }
 
 void NKJPLatticeReader::Worker::doRun() {
-    SET_LOGGING_LEVEL("DEBUG");
+    // SET_LOGGING_LEVEL("DEBUG");
     XmlPropertyTree xpt(inputStream_);
     BOOST_FOREACH(
         boost::property_tree::ptree::value_type &vP,
@@ -78,14 +78,13 @@ void NKJPLatticeReader::Worker::doRun() {
                         boost::property_tree::ptree::value_type &vSeg,
                         vS.second.get_child("")
                     ) {
-                        try {
-                            std::string segment(vSeg.second.get<std::string>("fs.f.string"));
-                            DEBUG(segment);
-                            appendSegmentToLattice_(segment);
-                        } catch (boost::property_tree::ptree_bad_data) {
-                            DEBUG("! bad data");
-                        } catch (boost::property_tree::ptree_bad_path) {
-                            DEBUG("! bad path");
+                        if (strcmp(vSeg.first.data(), "seg") == 0) {
+                            try {
+                                std::string segment(vSeg.second.get<std::string>("fs.f.string"));
+                                appendSegmentToLattice_(segment);
+                            } catch (boost::property_tree::ptree_error) {
+                                throw PsiException("nkjp-reader: input file error");
+                            }
                         }
                     }
                 }
@@ -99,17 +98,33 @@ void NKJPLatticeReader::Worker::appendSegmentToLattice_(std::string segment) {
     //temporary solution for inserting spaces
     std::locale loc;
     if (isalpha(segment.at(0), loc)) {
-        Lattice::VertexDescriptor sPrevEnd = lattice_.getLastVertex();
+        Lattice::VertexDescriptor sFrom = lattice_.getLastVertex();
         lattice_.appendStringWithSymbols(" ");
-        Lattice::VertexDescriptor sNowEnd = lattice_.getLastVertex();
+        Lattice::VertexDescriptor sTo = lattice_.getLastVertex();
         std::string blank(" ");
         AnnotationItem sItem("B", blank);
-        lattice_.addEdge(sPrevEnd, sNowEnd, sItem, textTags_);
+        Lattice::EdgeSequence::Builder sBuilder(lattice_);
+        sBuilder.addEdge(lattice_.firstOutEdge(
+            sFrom,
+            lattice_.getLayerTagManager().getMask("symbol")
+        ));
+        lattice_.addEdge(sFrom, sTo, sItem, textTags_, sBuilder.build());
     }
 
-    Lattice::VertexDescriptor prevEnd = lattice_.getLastVertex();
+    Lattice::VertexDescriptor from = lattice_.getLastVertex();
     lattice_.appendStringWithSymbols(segment);
-    Lattice::VertexDescriptor nowEnd = lattice_.getLastVertex();
+    Lattice::VertexDescriptor to = lattice_.getLastVertex();
     AnnotationItem item("seg", segment);
-    lattice_.addEdge(prevEnd, nowEnd, item, textTags_);
+    Lattice::EdgeSequence::Builder seqBuilder(lattice_);
+    Lattice::VertexDescriptor currentVertex = from;
+    Lattice::EdgeDescriptor currentEdge;
+    while (currentVertex != to) {
+        currentEdge = lattice_.firstOutEdge(
+            currentVertex,
+            lattice_.getLayerTagManager().getMask("symbol")
+        );
+        seqBuilder.addEdge(currentEdge);
+        currentVertex = lattice_.getEdgeTarget(currentEdge);
+    }
+    lattice_.addEdge(from, to, item, textTags_, seqBuilder.build());
 }
