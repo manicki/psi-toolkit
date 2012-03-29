27b746fbda5c22966d2d401a82c99fa855ffffa8
Paweł Skórzewski <pawels@uam5050>
2012-03-29 12:12:03 +0200

    Txt reader: reading empty lines fixed, test added

--- formats/txt/txt_lattice_reader.cpp
+++ formats/txt/txt_lattice_reader.cpp
@@ -63,12 +63,11 @@ void TxtLatticeReader::Worker::doRun() {
 }
 
 void TxtLatticeReader::Worker::appendParagraphToLattice_(std::string paragraph) {
-    Lattice::VertexDescriptor prevEnd = lattice_.getLastVertex();
-    lattice_.appendStringWithSymbols(paragraph);
-    Lattice::VertexDescriptor nowEnd = lattice_.getLastVertex();
-
-    AnnotationItem item("FRAG", paragraph);
-    lattice_.addEdge(prevEnd, nowEnd,
-                     item, textTags_);
-
+    if (!paragraph.empty()) {
+        Lattice::VertexDescriptor prevEnd = lattice_.getLastVertex();
+        lattice_.appendStringWithSymbols(paragraph);
+        Lattice::VertexDescriptor nowEnd = lattice_.getLastVertex();
+        AnnotationItem item("FRAG", paragraph);
+        lattice_.addEdge(prevEnd, nowEnd, item, textTags_);
+    }
 }
