bb01eca1af9685e495b73ef6a54e5c9429193481
Filip Graliński <filipg@amu.edu.pl>
2012-04-04 20:54:48 +0200

    fixes #24

--- utf8case/case_converter_factory.hpp
+++ utf8case/case_converter_factory.hpp
@@ -6,6 +6,8 @@
 #include "general_case_converter.hpp"
 #include "psi_exception.hpp"
 #include "regular_contextual_case_converter.hpp"
+#include "turkish_and_azeri_lower_contextual_case_converter.hpp"
+#include "turkish_and_azeri_upper_contextual_case_converter.hpp"
 
 const size_t NUMBER_OF_CASE_TYPES = 3;
 
@@ -23,7 +25,8 @@ private:
     boost::shared_ptr<SpecialCasingConverter> specialCasingConverters_[NUMBER_OF_CASE_TYPES];
 
     boost::shared_ptr<ContextualCaseConverter> regularContextualCaseConverter_;
-
+    boost::shared_ptr<ContextualCaseConverter> turkishAndAzeriUpperContextualCaseConverter_;
+    boost::shared_ptr<ContextualCaseConverter> turkishAndAzeriLowerContextualCaseConverter_;
 
     class Exception : public PsiException  {
     public:
@@ -89,9 +92,6 @@ private:
 
     boost::shared_ptr<GeneralCaseConverter<octet_iterator, output_iterator> > getCaseConverter_(
         int case_index, const std::string& language_code) {
-        if (language_code == "lt" || language_code == "az" || language_code == "tr")
-            throw Exception(std::string("language '") + language_code
-                            + "' is not handled yet in lower/upper/title-casing");
 
         checkRawConverters_(case_index);
 
@@ -99,12 +99,34 @@ private:
             new GeneralCaseConverter<octet_iterator, output_iterator> (
                 rangeBasedCaseConverters_[case_index],
                 specialCasingConverters_[case_index],
-                regularContextualCaseConverter_));
+                getContextualCaseConverterForLanguage_(language_code, case_index)));
     }
 
 public:
-    CaseConverterFactory():regularContextualCaseConverter_(
-        boost::shared_ptr<ContextualCaseConverter>(new RegularContextualCaseConverter())) {
+    CaseConverterFactory():
+        regularContextualCaseConverter_(
+            boost::shared_ptr<ContextualCaseConverter>(new RegularContextualCaseConverter())),
+        turkishAndAzeriUpperContextualCaseConverter_(
+            boost::shared_ptr<ContextualCaseConverter>(
+                new TurkishAndAzeriUpperContextualCaseConverter())),
+        turkishAndAzeriLowerContextualCaseConverter_(
+            boost::shared_ptr<ContextualCaseConverter>(
+                new TurkishAndAzeriLowerContextualCaseConverter())) {
+    }
+
+    boost::shared_ptr<ContextualCaseConverter> getContextualCaseConverterForLanguage_(
+        const std::string& languageCode, int caseIndex) {
+        if (languageCode == "lt")
+            throw Exception(std::string("language '") + languageCode
+                            + "' is not handled yet in lower/upper/title-casing");
+
+        if (languageCode == "tr" || languageCode == "az")
+            return
+                caseIndex == LOWER_INDEX
+                ? turkishAndAzeriLowerContextualCaseConverter_
+                : turkishAndAzeriUpperContextualCaseConverter_;
+
+        return regularContextualCaseConverter_;
     }
 
     boost::shared_ptr<GeneralCaseConverter<octet_iterator, output_iterator> >
