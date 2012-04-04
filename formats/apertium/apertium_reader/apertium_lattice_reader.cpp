55a4df607d97514079764226560b5ffc7b0a842f
Roman Grundkiewicz <romang@amu.edu.pl>
2012-04-04 11:10:26 +0200

    adding apertium-reader results into lattice

--- formats/apertium/apertium_reader/apertium_lattice_reader.cpp
+++ formats/apertium/apertium_reader/apertium_lattice_reader.cpp
@@ -1,4 +1,5 @@
 #include <boost/algorithm/string/trim.hpp>
+#include <boost/assign/list_of.hpp>
 
 #include "apertium_lattice_reader.hpp"
 #include "processor_file_fetcher.cpp"
@@ -12,10 +13,13 @@ std::string ApertiumLatticeReader::doInfo() {
     return "Apertium reader";
 }
 
-std::string ApertiumLatticeReader::deformat(const std::string& input) {
-    return apertiumDeformatter_.deformat(input);
+std::string ApertiumLatticeReader::processReplacementRules(const std::string& input) {
+    return apertiumDeformatter_.processReplacementRules(input);
 }
 
+std::vector<DeformatIndex> ApertiumLatticeReader::processFormatRules(const std::string& input) {
+    return apertiumDeformatter_.processFormatRules(input);
+}
 
 const std::string ApertiumLatticeReader::Factory::DEFAULT_SPEC_FILES_DIR = "%ITSDATA%/";
 const std::string ApertiumLatticeReader::Factory::DEFAULT_SPEC_FILE_ENDING = "-format.xml";
@@ -67,8 +71,13 @@ boost::filesystem::path ApertiumLatticeReader::Factory::doGetFile() {
 
 ApertiumLatticeReader::Worker::Worker(ApertiumLatticeReader& processor,
                                       std::istream& inputStream,
-                                      Lattice& lattice)
-    : ReaderWorker<std::istream>(inputStream, lattice), processor_(processor) {
+                                      Lattice& lattice) :
+    ReaderWorker<std::istream>(inputStream, lattice),
+    processor_(processor),
+    fragTags_(lattice_.getLayerTagManager().createTagCollectionFromList(
+        boost::assign::list_of("frag")("apertium-reader"))),
+    tagTags_(lattice_.getLayerTagManager().createTagCollectionFromList(
+        boost::assign::list_of("tag")("apertium-reader"))) {
 }
 
 void ApertiumLatticeReader::Worker::doRun() {
@@ -81,9 +90,61 @@ void ApertiumLatticeReader::Worker::doRun() {
         if (boost::algorithm::trim_copy(line).empty()) {
             continue;
         }
-        input += line + "\n";
+        input += line;// + "\n";
     }
 
-    std::string output = processor_.deformat(input);
+    input = processor_.processReplacementRules(input);
+    std::vector<DeformatIndex> indexes = processor_.processFormatRules(input);
+
+    if (indexes.size() != 0) {
+        std::string text;
+        unsigned int length;
+
+        if (indexes[0].begin != 0) {
+            text = input.substr(0, indexes[0].begin);
+            DEBUG("frag: " << text);
+            appendFragmentToLattice_(text);
+        }
+
+        for (unsigned int i = 0; i < indexes.size(); i++) {
+            text = input.substr(indexes[i].begin, indexes[i].length());
+            DEBUG("tag: " << text << " [" << indexes[i].type << ", " << indexes[i].eos << "]");
+            appendTagToLattice_(text, indexes[i].type, indexes[i].eos);
+
+            length = (i == indexes.size() - 1) ? input.length() : indexes[i+1].begin;
+            length -= indexes[i].end;
+
+            if (length != 0) {
+                text = input.substr(indexes[i].end, length);
+                DEBUG("frag: " << text);
+                appendFragmentToLattice_(text);
+            }
+        }
+
+    }
+
+    std::string output = "";
     DEBUG(output);
+}
+
+void ApertiumLatticeReader::Worker::appendFragmentToLattice_(std::string fragment) {
+    Lattice::VertexDescriptor prevEnd = lattice_.getLastVertex();
+    lattice_.appendStringWithSymbols(fragment);
+    Lattice::VertexDescriptor nowEnd = lattice_.getLastVertex();
+    AnnotationItem item("FRAG", fragment);
+    lattice_.addEdge(prevEnd, nowEnd, item, fragTags_);
+}
+
+void ApertiumLatticeReader::Worker::appendTagToLattice_(
+    std::string tag, std::string type, bool eos) {
+
+    Lattice::VertexDescriptor prevEnd = lattice_.getLastVertex();
+    lattice_.appendStringWithSymbols(tag);
+    Lattice::VertexDescriptor nowEnd = lattice_.getLastVertex();
+
+    AnnotationItem item(type + "-tag", StringFrag(tag));
+    AnnotationItemManager& manager = lattice_.getAnnotationItemManager();
+    manager.setValue(item, "eos", eos ? "yes" : "no");
+
+    lattice_.addEdge(prevEnd, nowEnd, item, tagTags_);
 }
