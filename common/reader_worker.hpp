4bc6c9ee4cf4f1c34020796cd65cfbc3c0069a13
Paweł Skórzewski <pawels@uam5050>
2012-04-10 11:40:55 +0200

    ReaderWorker's method appendParagraphToLattice moved

--- common/reader_worker.hpp
+++ common/reader_worker.hpp
@@ -19,16 +19,24 @@ public:
 protected:
     std::istream& inputStream_;
 
-    void appendParagraphToLattice_(std::string paragraph, LayerTagCollection tags) {
-        if (!paragraph.empty()) {
-            Lattice::VertexDescriptor prevEnd = lattice_.getLastVertex();
-            lattice_.appendStringWithSymbols(paragraph);
-            Lattice::VertexDescriptor nowEnd = lattice_.getLastVertex();
-            AnnotationItem item("FRAG", paragraph);
-            lattice_.addEdge(prevEnd, nowEnd, item, tags);
-        }
-    }
+    void appendParagraphToLattice_(std::string paragraph, LayerTagCollection tags);
 
 };
+
+
+template<typename Source>
+void ReaderWorker<Source>::appendParagraphToLattice_(
+    std::string paragraph,
+    LayerTagCollection tags
+) {
+    if (!paragraph.empty()) {
+        Lattice::VertexDescriptor prevEnd = lattice_.getLastVertex();
+        lattice_.appendStringWithSymbols(paragraph);
+        Lattice::VertexDescriptor nowEnd = lattice_.getLastVertex();
+        AnnotationItem item("FRAG", paragraph);
+        lattice_.addEdge(prevEnd, nowEnd, item, tags);
+    }
+}
+
 
 #endif
