d8d2fb0a9d8b687caeae4450872cf73605ed06f0
Paweł Skórzewski <pawels@uam5050>
2012-05-17 14:03:34 +0200

    Active elements printer's methods

--- formats/bracketing/bracketing-writer/active_elements_printer.cpp
+++ formats/bracketing/bracketing-writer/active_elements_printer.cpp
@@ -1,1 +1,51 @@
 #include "active_elements_printer.hpp"
+
+
+#include <sstream>
+
+#include <boost/foreach.hpp>
+
+
+ActiveElementsPrinter ActiveElementsPrinter::Manager::getPrinter(std::string pattern) {
+    return ActiveElementsPrinter(
+        pattern,
+        tagSeparator_
+    );
+}
+
+
+std::string ActiveElementsPrinter::print(
+    std::list<std::string> tags
+) {
+    std::stringstream result;
+    size_t i = 0;
+    while (i < pattern_.length()) {
+        if (pattern_[i] == '%' && i+1 < pattern_.length()) {
+            switch (pattern_[i+1]) {
+            case 'T' :
+                {
+                    std::stringstream tagSs;
+                    BOOST_FOREACH(std::string tag, tags) {
+                        if (!tagSs.str().empty()) {
+                            tagSs << tagSeparator_;
+                        }
+                        tagSs << tag;
+                    }
+                    result << tagSs.str();
+                    i += 2;
+                }
+                break;
+            default :
+                {
+                    result << '%';
+                    i++;
+                }
+                break;
+            }
+        } else {
+            result << pattern_[i];
+            i++;
+        }
+    }
+    return result.str();
+}
--- formats/bracketing/bracketing-writer/active_elements_printer.hpp
+++ formats/bracketing/bracketing-writer/active_elements_printer.hpp
@@ -2,11 +2,44 @@
 #define ACTIVE_ELEMENTS_PRINTER_HDR
 
 
+#include <list>
+#include <string>
+
+
 class ActiveElementsPrinter {
 
 public:
 
+    class Manager {
+    public:
+        Manager(
+            std::string tagSeparator
+        ) :
+            tagSeparator_(tagSeparator)
+        { }
+
+        ActiveElementsPrinter getPrinter(std::string pattern);
+
+    private:
+        std::string tagSeparator_;
+    };
+
+    ActiveElementsPrinter(
+        std::string pattern,
+        std::string tagSeparator
+    ) :
+        pattern_(pattern),
+        tagSeparator_(tagSeparator)
+    { }
+
+    std::string print(
+        std::list<std::string> tags
+    );
+
 private:
+
+    std::string pattern_;
+    std::string tagSeparator_;
 
 };
 
