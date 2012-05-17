504a6a643297b87954b4e8f8dff1121a3d7771d4
Paweł Skórzewski <pawels@uam5050>
2012-05-17 14:18:34 +0200

    Active elements printer's test

--- formats/bracketing/bracketing-writer/t/active_elements_printer_tests.cpp
+++ formats/bracketing/bracketing-writer/t/active_elements_printer_tests.cpp
@@ -1,16 +1,21 @@
 #include "tests.hpp"
 
-#include "bracketing_lattice_writer.hpp"
+#include <boost/assign.hpp>
+
+#include "active_elements_printer.hpp"
 
 
 BOOST_AUTO_TEST_SUITE( active_elements_printer )
 
 
 BOOST_AUTO_TEST_CASE( active_elements_printer_simple ) {
-
-    BOOST_CHECK(true);
-    BOOST_CHECK_EQUAL(0, 0);
-
+    ActiveElementsPrinter::Manager aepManager(",");
+    ActiveElementsPrinter aep = aepManager.getPrinter("<%c tags=\"%T\" score=\"%s\">%t</%c> % %");
+    std::list<std::string> tags = boost::assign::list_of("symbol")("token")("segment");
+    BOOST_CHECK_EQUAL(
+        aep.print(tags),
+        "<%c tags=\"symbol,token,segment\" score=\"%s\">%t</%c> % %"
+    );
 }
 
 
