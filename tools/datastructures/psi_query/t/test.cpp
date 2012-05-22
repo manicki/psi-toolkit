7e732a4bf97b0a8fa6020e4e1a26aa81bd2a7af9
marcinj <junczys@amu.edu.pl>
2012-05-22 10:33:48 +0200

    Fixed compilation error

--- tools/datastructures/psi_query/t/test.cpp
+++ tools/datastructures/psi_query/t/test.cpp
@@ -13,21 +13,21 @@ BOOST_AUTO_TEST_CASE(DFSA_test)
     psi::State q2 = fsa.addState();
     psi::State q3 = fsa.addState();
 
-    fsa.addArc(q0, psi::Arc<>('a', q1));
-    fsa.addArc(q0, psi::Arc<>('b', q1));
-    fsa.addArc(q0, psi::Arc<>('c', q1));
+    fsa.addArc(q0, psi::ArcWeighted<>('a', q1));
+    fsa.addArc(q0, psi::ArcWeighted<>('b', q1));
+    fsa.addArc(q0, psi::ArcWeighted<>('c', q1));
 
-    fsa.addArc(q1, psi::Arc<>('a', q1));
-    fsa.addArc(q1, psi::Arc<>('b', q2));
-    fsa.addArc(q1, psi::Arc<>('c', q3));
+    fsa.addArc(q1, psi::ArcWeighted<>('a', q1));
+    fsa.addArc(q1, psi::ArcWeighted<>('b', q2));
+    fsa.addArc(q1, psi::ArcWeighted<>('c', q3));
 
-    fsa.addArc(q2, psi::Arc<>('a', q1));
-    fsa.addArc(q2, psi::Arc<>('b', q3));
-    fsa.addArc(q2, psi::Arc<>('c', q3));
+    fsa.addArc(q2, psi::ArcWeighted<>('a', q1));
+    fsa.addArc(q2, psi::ArcWeighted<>('b', q3));
+    fsa.addArc(q2, psi::ArcWeighted<>('c', q3));
 
-    fsa.addArc(q3, psi::Arc<>('a', q1));
-    fsa.addArc(q3, psi::Arc<>('b', q3));
-    fsa.addArc(q3, psi::Arc<>('c', q3));
+    fsa.addArc(q3, psi::ArcWeighted<>('a', q1));
+    fsa.addArc(q3, psi::ArcWeighted<>('b', q3));
+    fsa.addArc(q3, psi::ArcWeighted<>('c', q3));
 
 
     fsa.setStartState(q0);
