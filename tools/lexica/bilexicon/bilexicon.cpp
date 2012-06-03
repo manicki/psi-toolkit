1ccb35eeffef4b4a2943523a60ab9bf42bed5fc1
Filip Graliński <filipg@amu.edu.pl>
2012-06-03 22:56:25 +0200

    use LexiconBase in BiLexicon

--- tools/lexica/bilexicon/bilexicon.cpp
+++ tools/lexica/bilexicon/bilexicon.cpp
@@ -40,7 +40,7 @@ BiLexicon::BiLexicon(const boost::program_options::variables_map& options) {
     }
 
     if (options.count("save-binary-lexicon") > 0) {
-        if (!store_)
+        if (lexiconBase_.isEmpty())
             throw new Exception("no data to save");
 
         boost::filesystem::path binaryLexiconPath(
@@ -140,15 +140,10 @@ std::list<std::string> BiLexicon::tagsToOperateOn() {
 void BiLexicon::processEdge(Lattice& lattice, Lattice::EdgeDescriptor edge) {
     std::string edgeText = lattice.getAnnotationText(edge);
 
-    boost::optional<std::string> entryFound = store_->get(edgeText);
+    std::vector<std::string> records = lexiconBase_.getRecords(edgeText);
 
-    if (entryFound) {
-        std::vector<std::string> records;
-        boost::split(records, entryFound.get(), boost::is_any_of(";"));
-
-        BOOST_FOREACH(std::string& record, records) {
-            addEntry_(lattice, edge, record);
-        }
+    BOOST_FOREACH(std::string& record, records) {
+        addEntry_(lattice, edge, record);
     }
 }
 
@@ -181,82 +176,15 @@ AnnotationItem BiLexicon::parseRecord_(const std::string& record) {
 }
 
 void BiLexicon::readPlainText_(const boost::filesystem::path& plainTextLexicon) {
-
-    KeyValueStore::Builder storeBuilder;
-
-    boost::filesystem::ifstream plainTextStream(plainTextLexicon);
-
-    std::string line;
-    std::string prevKey;
-    std::string stringSoFar;
-    bool firstEntry = true;
-
-    while (std::getline(plainTextStream, line)) {
-        removeComment_(line);
-
-        if (isEmptyLine_(line))
-            continue;
-
-        std::vector<std::string> fields;
-        boost::split(fields, line, boost::is_any_of("\t "));
-        fields.erase(std::remove_if(
-                         fields.begin(), fields.end(),
-                         boost::bind( &std::string::empty, _1 )), fields.end());
-
-        BOOST_FOREACH(std::string& field, fields) {
-            field = quoter_.unescape(field);
-        }
-
-        if (fields.size() != 2)
-            throw Exception(
-                std::string("two fields expected in plain text bilexicon, was: `")
-                + line + "`");
-
-        if (firstEntry) {
-            prevKey = fields[0];
-            stringSoFar = fields[1];
-            firstEntry = false;
-        } else {
-            if (fields[0] == prevKey) {
-                stringSoFar += ";";
-                stringSoFar += fields[1];
-            }
-            else {
-                storeBuilder.add(prevKey, stringSoFar);
-                prevKey = fields[0];
-                stringSoFar = fields[1];
-            }
-        }
-    }
-
-    if (!firstEntry)
-        storeBuilder.add(prevKey, stringSoFar);
-
-    store_.reset(storeBuilder.build());
+    lexiconBase_.readPlainText(plainTextLexicon);
 }
 
 void BiLexicon::saveBinary_(const boost::filesystem::path& binaryLexiconPath) {
-    WARN("saving lexicon to `" << binaryLexiconPath.string() << "`");
-
-    store_->save(binaryLexiconPath.string());
-
-    WARN("lexicon saved");
+    lexiconBase_.saveBinary(binaryLexiconPath);
 }
 
 void BiLexicon::loadBinary_(const boost::filesystem::path& binaryLexiconPath) {
-    store_.reset(new KeyValueStore());
-    store_->load(binaryLexiconPath.string());
-}
-
-void BiLexicon::removeComment_(std::string& s) {
-    size_t hashPos = s.find_first_of('#');
-
-    if (hashPos != std::string::npos)
-        s = s.substr(0, hashPos);
-}
-
-bool BiLexicon::isEmptyLine_(const std::string& s) {
-    return s.find_first_not_of(" \t");
+    lexiconBase_.loadBinary(binaryLexiconPath);
 }
 
 void BiLexicon::createTags_(const std::string& trg_lang) {
