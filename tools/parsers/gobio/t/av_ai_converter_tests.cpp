c84e7055f51e3ab254c3ca05d2591aa792a4c3b4
Paweł Skórzewski <pawels@uam5050>
2012-06-14 12:57:12 +0200

    AV-AI converter's tests added

---
+++ tools/parsers/gobio/t/av_ai_converter_tests.cpp
@@ -0,0 +1,85 @@
+#include "tests.hpp"
+
+#include "annotation_item.hpp"
+#include "av_ai_converter.hpp"
+#include "av_matrix.hpp"
+#include "lattice.hpp"
+#include "zvalue.hpp"
+
+
+BOOST_AUTO_TEST_SUITE( av_ai_converter )
+
+
+BOOST_AUTO_TEST_CASE( conversion_int_av_to_ai ) {
+
+    Lattice lattice;
+    AV_AI_Converter converter(lattice);
+    av_matrix<int, int> avm(0);
+    avm.set_attr(1, 2, -1L);
+    avm.set_attr(3, 4, -1L);
+    AnnotationItem ai = converter.toAnnotationItem(avm);
+    BOOST_CHECK_EQUAL(ai.getCategory(), "0");
+    BOOST_CHECK_EQUAL(lattice.getAnnotationItemManager().getValueAsString(ai, "1"), "2");
+    BOOST_CHECK_EQUAL(lattice.getAnnotationItemManager().getValueAsString(ai, "3"), "4");
+
+    av_matrix<int, int> avm2 = converter.toAVMatrix< av_matrix<int, int> >(ai);
+    BOOST_CHECK_EQUAL(avm2.get_cat(), 0);
+    BOOST_CHECK_EQUAL(avm2.get_attr(1), 2);
+    BOOST_CHECK_EQUAL(avm2.get_attr(3), 4);
+
+}
+
+
+BOOST_AUTO_TEST_CASE( conversion_int_ai_to_av ) {
+
+    Lattice lattice;
+    AV_AI_Converter converter(lattice);
+    AnnotationItem ai("0");
+    lattice.getAnnotationItemManager().setValue(ai, "1", INTEGER_TO_ZVALUE(2));
+    lattice.getAnnotationItemManager().setValue(ai, "3", INTEGER_TO_ZVALUE(4));
+    av_matrix<int, int> avm = converter.toAVMatrix< av_matrix<int, int> >(ai);
+    BOOST_CHECK_EQUAL(avm.get_cat(), 0);
+    BOOST_CHECK_EQUAL(avm.get_attr(1), 2);
+    BOOST_CHECK_EQUAL(avm.get_attr(3), 4);
+
+    AnnotationItem ai2 = converter.toAnnotationItem(avm);
+    BOOST_CHECK_EQUAL(ai2.getCategory(), "0");
+    BOOST_CHECK_EQUAL(lattice.getAnnotationItemManager().getValueAsString(ai2, "1"), "2");
+    BOOST_CHECK_EQUAL(lattice.getAnnotationItemManager().getValueAsString(ai2, "3"), "4");
+
+}
+
+/*
+BOOST_AUTO_TEST_CASE( conversion_partially_string ) {
+
+    Lattice lattice;
+    AV_AI_Converter converter(lattice);
+    AnnotationItem ai("0");
+    lattice.getAnnotationItemManager().setValue(ai, "1", "bbb");
+    lattice.getAnnotationItemManager().setValue(ai, "3", "ddd");
+    av_matrix<int, int> avm = converter.toAVMatrix< av_matrix<int, int> >(ai);
+    AnnotationItem ai2 = converter.toAnnotationItem(avm);
+    BOOST_CHECK_EQUAL(ai2.getCategory(), "0");
+    BOOST_CHECK_EQUAL(lattice.getAnnotationItemManager().getValueAsString(ai2, "1"), "bbb");
+    BOOST_CHECK_EQUAL(lattice.getAnnotationItemManager().getValueAsString(ai2, "3"), "ddd");
+
+}
+
+
+BOOST_AUTO_TEST_CASE( conversion_string ) {
+
+    Lattice lattice;
+    AV_AI_Converter converter(lattice);
+    AnnotationItem ai("category");
+    lattice.getAnnotationItemManager().setValue(ai, "Aa", "bbb");
+    lattice.getAnnotationItemManager().setValue(ai, "Cc", "ddd");
+    av_matrix<int, int> avm = converter.toAVMatrix< av_matrix<int, int> >(ai);
+    AnnotationItem ai2 = converter.toAnnotationItem(avm);
+    BOOST_CHECK_EQUAL(ai2.getCategory(), "category");
+    BOOST_CHECK_EQUAL(lattice.getAnnotationItemManager().getValueAsString(ai2, "Aa"), "bbb");
+    BOOST_CHECK_EQUAL(lattice.getAnnotationItemManager().getValueAsString(ai2, "Cc"), "ddd");
+
+}
+*/
+
+BOOST_AUTO_TEST_SUITE_END()
